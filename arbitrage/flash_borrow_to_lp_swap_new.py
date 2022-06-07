34569e7707af65b7b303726267de427ca77bb974
BowTiedDevil <devil@bowtieddevil.com>
2022-06-06 20:01:52 -0700

    adjust type hint

--- arbitrage/flash_borrow_to_lp_swap_new.py
+++ arbitrage/flash_borrow_to_lp_swap_new.py
@@ -126,7 +126,7 @@ class FlashBorrowToLpSwapNew:
 
     def _build_multipool_amounts_out(
         self, token_in: Erc20Token, token_in_quantity: int
-    ) -> List[list]:
+    ) -> List[list[int, int]]:
 
         number_of_pools = len(self.swap_pools)
 
