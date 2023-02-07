aff9b1cc66b35d121253da1abacdf11ed2a31d6e
BowTiedDevil <devil@bowtieddevil.com>
2023-02-06 22:21:34 -0800

    add minimum ERC-20 ABI

--- token.py
+++ token.py
@@ -8,7 +8,7 @@ from brownie.network.account import LocalAccount
 
 from degenbot.chainlink import ChainlinkPriceContract
 
-ERC20 = json.loads(
+MIN_ERC20_ABI = json.loads(
     """
     [{"constant": true, "inputs": [], "name": "name", "outputs": [ { "name": "", "type": "string" } ], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [ { "name": "_spender", "type": "address" }, { "name": "_value", "type": "uint256" } ], "name": "approve", "outputs": [ { "name": "", "type": "bool" } ], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": true, "inputs": [], "name": "totalSupply", "outputs": [ { "name": "", "type": "uint256" } ], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [ { "name": "_from", "type": "address" }, { "name": "_to", "type": "address" }, { "name": "_value", "type": "uint256" } ], "name": "transferFrom", "outputs": [ { "name": "", "type": "bool" } ], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": true, "inputs": [], "name": "decimals", "outputs": [ { "name": "", "type": "uint8" } ], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": true, "inputs": [ { "name": "_owner", "type": "address" } ], "name": "balanceOf", "outputs": [ { "name": "balance", "type": "uint256" } ], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": true, "inputs": [], "name": "symbol", "outputs": [ { "name": "", "type": "string" } ], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [ { "name": "_to", "type": "address" }, { "name": "_value", "type": "uint256" } ], "name": "transfer", "outputs": [ { "name": "", "type": "bool" } ], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": true, "inputs": [ { "name": "_owner", "type": "address" }, { "name": "_spender", "type": "address" } ], "name": "allowance", "outputs": [ { "name": "", "type": "uint256" } ], "payable": false, "stateMutability": "view", "type": "function" }, { "payable": true, "stateMutability": "payable", "type": "fallback" }, { "anonymous": false, "inputs": [ { "indexed": true, "name": "owner", "type": "address" }, { "indexed": true, "name": "spender", "type": "address" }, { "indexed": false, "name": "value", "type": "uint256" } ], "name": "Approval", "type": "event" }, { "anonymous": false, "inputs": [ { "indexed": true, "name": "from", "type": "address" }, { "indexed": true, "name": "to", "type": "address" }, { "indexed": false, "name": "value", "type": "uint256" } ], "name": "Transfer", "type": "event"}]
     """
@@ -30,6 +30,7 @@ class Erc20Token:
         oracle_address: str = None,
         silent: bool = False,
         unload_brownie_contract_after_init: bool = False,
+        min_abi: bool = False,
     ) -> None:
 
         try:
@@ -43,26 +44,31 @@ class Erc20Token:
         if user:
             self._user = user
 
-        with catch_warnings():
-            simplefilter("ignore")
-            try:
-                # attempt to load stored contract
-                self._contract = Contract(self.address)
-            except:
-                # use the provided ABI if given
-                if abi:
-                    try:
-                        self._contract = Contract.from_abi(
-                            name="", address=self.address, abi=abi
-                        )
-                    except:
-                        raise
-                # otherwise attempt to fetch from the block explorer
-                else:
-                    try:
-                        self._contract = Contract.from_explorer(address)
-                    except:
-                        raise
+        if min_abi:
+            self._contract = Contract.from_abi(
+                name="", address=self.address, abi=MIN_ERC20_ABI
+            )
+        else:
+            with catch_warnings():
+                simplefilter("ignore")
+                try:
+                    # attempt to load stored contract
+                    self._contract = Contract(self.address)
+                except:
+                    # use the provided ABI if given
+                    if abi:
+                        try:
+                            self._contract = Contract.from_abi(
+                                name="", address=self.address, abi=abi
+                            )
+                        except:
+                            raise
+                    # otherwise attempt to fetch from the block explorer
+                    else:
+                        try:
+                            self._contract = Contract.from_explorer(address)
+                        except:
+                            raise
 
         if "name" in dir(self._contract):
             self.name = self._contract.name()
