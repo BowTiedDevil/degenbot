e274b3784f0a7710c2d37a16747d49928fbbc0b2
BowTiedDevil <devil@bowtieddevil.com>
2023-05-12 20:10:37 -0700

    sort imports, remove unnecessary type hints

--- uniswap/v3/libraries/TickBitmap.py
+++ uniswap/v3/libraries/TickBitmap.py
@@ -1,14 +1,13 @@
-from typing import Tuple, Optional
+from decimal import Decimal
+from typing import Optional, Tuple
 
 from degenbot.exceptions import (
-    MissingTickWordError,
     BitmapWordUnavailableError,
+    MissingTickWordError,
 )
 
 from . import BitMath
 from .Helpers import *
-
-from decimal import Decimal
 
 
 def flipTick(
@@ -17,7 +16,6 @@ def flipTick(
     tickSpacing: int,
     update_block: Optional[int] = None,
 ):
-
     if not (tick % tickSpacing == 0):
         raise EVMRevertError("Tick not correctly spaced!")
 
@@ -51,7 +49,6 @@ def nextInitializedTickWithinOneWord(
     tickSpacing: int,
     lte: bool,
 ) -> Tuple[int, bool]:
-
     compressed: int = int(
         Decimal(tick) // tickSpacing
     )  # tick can be negative, use Decimal so floor division rounds to zero instead of negative infinity
@@ -61,17 +58,17 @@ def nextInitializedTickWithinOneWord(
     if lte:
         wordPos, bitPos = position(compressed)
         # all the 1s at or to the right of the current bitPos
-        mask: int = (1 << bitPos) - 1 + (1 << bitPos)
+        mask = (1 << bitPos) - 1 + (1 << bitPos)
 
         try:
             bitmap_word = tickBitmap[wordPos]["bitmap"]
         except:
             raise BitmapWordUnavailableError(wordPos)
         else:
-            masked: int = bitmap_word & mask
+            masked = bitmap_word & mask
 
         # if there are no initialized ticks to the right of or at the current tick, return rightmost in the word
-        initialized_status: bool = masked != 0
+        initialized_status = masked != 0
         # overflow/underflow is possible, but prevented externally by limiting both tickSpacing and tick
         next_tick = (
             (compressed - int24(bitPos - BitMath.mostSignificantBit(masked)))
@@ -83,17 +80,17 @@ def nextInitializedTickWithinOneWord(
         # start from the word of the next tick, since the current tick state doesn't matter
         wordPos, bitPos = position(compressed + 1)
         # all the 1s at or to the left of the bitPos
-        mask: int = ~((1 << bitPos) - 1)
+        mask = ~((1 << bitPos) - 1)
 
         try:
             bitmap_word = tickBitmap[wordPos]["bitmap"]
         except:
             raise BitmapWordUnavailableError(wordPos)
         else:
-            masked: int = bitmap_word & mask
+            masked = bitmap_word & mask
 
         # if there are no initialized ticks to the left of the current tick, return leftmost in the word
-        initialized_status: bool = masked != 0
+        initialized_status = masked != 0
         # overflow/underflow is possible, but prevented externally by limiting both tickSpacing and tick
         next_tick = (
             (
