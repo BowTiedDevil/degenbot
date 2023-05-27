612249f416baba7ac08eefd6a391c49317683a3e
BowTiedDevil <devil@bowtieddevil.com>
2023-05-27 15:35:27 -0700

    set logger defaults

--- logging.py
+++ logging.py
@@ -1,3 +1,9 @@
 import logging
 
+"""
+Create a shared logger instance for this module. Set default level to INFO and send to stdout with StreamHandler
+"""
+
 logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+logger.addHandler(logging.StreamHandler())
