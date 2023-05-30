d53850a5da335daf2092b73ec04141659f69d768
BowTiedDevil <devil@bowtieddevil.com>
2023-05-30 16:07:50 -0700

    remove unused version key

--- transaction/uniswap_transaction.py
+++ transaction/uniswap_transaction.py
@@ -37,35 +37,30 @@ _ROUTERS: Dict[int, Dict[str, Dict]] = {
     1: {
         "0xd9e1cE17f2641f24aE83637ab66a2cca9C378B9F": {
             "name": "Sushiswap: Router",
-            "uniswap_version": 2,
             "factory_address": {
                 2: "0xC0AEe478e3658e2610c5F7A4A2E1777cE9e4f2Ac"
             },
         },
         "0xf164fC0Ec4E93095b804a4795bBe1e041497b92a": {
             "name": "UniswapV2: Router",
-            "uniswap_version": 2,
             "factory_address": {
                 2: "0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f"
             },
         },
         "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D": {
             "name": "UniswapV2: Router 2",
-            "uniswap_version": 2,
             "factory_address": {
                 2: "0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f"
             },
         },
         "0xE592427A0AEce92De3Edee1F18E0157C05861564": {
             "name": "UniswapV3: Router",
-            "uniswap_version": 3,
             "factory_address": {
                 3: "0x1F98431c8aD98523631AE4a59f267346ea31F984"
             },
         },
         "0x68b3465833fb72A70ecDF485E0e4C7bD8665Fc45": {
             "name": "UniswapV3: Router 2",
-            "uniswap_version": 3,
             "factory_address": {
                 2: "0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f",
                 3: "0x1F98431c8aD98523631AE4a59f267346ea31F984",
@@ -73,10 +68,6 @@ _ROUTERS: Dict[int, Dict[str, Dict]] = {
         },
         "0xEf1c6E67703c7BD7107eed8303Fbe6EC2554BF6B": {
             "name": "Uniswap Universal Router",
-            # TODO: determine if 'uniswap_version' is necessary,
-            # or convert to tuple (2,3) so routers that support
-            # two or more versions can be handled correctly
-            "uniswap_version": 3,
             "factory_address": {
                 2: "0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f",
                 3: "0x1F98431c8aD98523631AE4a59f267346ea31F984",
@@ -237,15 +228,26 @@ class UniswapTransaction(Transaction):
 
         The `router_dict` argument should contain at minimum the following key-value pairs:
             - 'name': [str]
-            - 'uniswap_version': [int]
             - 'factory_address': {
                 [int]: [address],
                 [int]: [address],
-            },
+            }
+
+            The dicts inside 'factory_address' are keyed by the Uniswap version associated with their contracts, e.g.
+            router_dict = {
+                'name': 'SomeDEX',
+                'factory_address': {
+                    2: '0x...',
+                    3: '0x...',
+                }
+            }
         """
         router_address = Web3.toChecksumAddress(router_address)
 
-        for key in ["name", "uniswap_version", "factory_address"]:
+        for key in [
+            "name",
+            "factory_address",
+        ]:
             if key not in router_dict:
                 raise ValueError(f"{key} not found in router_dict")
 
