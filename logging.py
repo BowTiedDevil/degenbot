164dc1f782217e95e4fce2d8893c094d72bb8277
BowTiedDevil <devil@bowtieddevil.com>
2023-06-03 00:11:31 -0700

    use explicit name for logger

--- logging.py
+++ logging.py
@@ -4,6 +4,6 @@ import logging
 Create a shared logger instance for this module. Set default level to INFO and send to stdout with StreamHandler
 """
 
-logger = logging.getLogger(__name__)
+logger = logging.getLogger("degenbot")
 logger.setLevel(logging.INFO)
 logger.addHandler(logging.StreamHandler())
