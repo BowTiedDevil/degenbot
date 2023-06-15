55cf27e81539417e62deb383aa3db091b3c6367b
BowTiedDevil <devil@bowtieddevil.com>
2023-06-15 16:56:30 -0700

    consolidate base types

--- transaction/base.py
+++
@@ -1,5 +0,0 @@
-from abc import ABC
-
-
-class Transaction(ABC):
-    pass
---
+++ types.py
@@ -0,0 +1,17 @@
+from abc import ABC
+
+
+class ArbitrageHelper(ABC):
+    pass
+
+
+class PoolHelper(ABC):
+    pass
+
+
+class TokenHelper(ABC):
+    pass
+
+
+class TransactionHelper(ABC):
+    pass
