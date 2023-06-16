db8f9038d4fd9d119f2f2abc1992d880ae743643
BowTiedDevil <devil@bowtieddevil.com>
2023-06-15 17:21:48 -0700

    sort v3 imports

--- uniswap/__init__.py
+++ uniswap/__init__.py
@@ -1,14 +1,21 @@
 from degenbot.uniswap.abi import (
+    UNISWAPV2_ROUTER,
+)  # alias for older scripts that may be floating around
+from degenbot.uniswap.abi import (
     CAMELOT_LP_ABI,
     SUSHISWAP_LP_ABI,
     UNISWAP_UNIVERSAL_ROUTER_ABI,
-    UNISWAPV2_FACTORY_ABI,
-    UNISWAPV2_ROUTER_ABI,
-    UNISWAPV2_ROUTER,  # alias for older scripts that may be floating around
-    UNISWAPV2_LP_ABI,
-    UNISWAP_V3_TICKLENS_ABI,
     UNISWAP_V3_FACTORY_ABI,
     UNISWAP_V3_POOL_ABI,
-    UNISWAP_V3_ROUTER_ABI,
     UNISWAP_V3_ROUTER2_ABI,
+    UNISWAP_V3_ROUTER_ABI,
+    UNISWAP_V3_TICKLENS_ABI,
+    UNISWAPV2_FACTORY_ABI,
+    UNISWAPV2_LP_ABI,
+    UNISWAPV2_ROUTER_ABI,
+)
+
+from .uniswap_managers import (
+    UniswapV2LiquidityPoolManager,
+    UniswapV3LiquidityPoolManager,
 )
---
+++ uniswap/v3/__init__.py
@@ -0,0 +1,3 @@
+from . import functions, libraries
+from .tick_lens import TickLens
+from .v3_liquidity_pool import V3LiquidityPool
