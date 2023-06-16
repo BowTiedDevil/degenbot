dafcc9b50106fe6f9e146ab1c34029a7f2d8f023
BowTiedDevil <devil@bowtieddevil.com>
2023-06-16 09:07:26 -0700

    ABI alias (workaround for old scripts)

--- uniswap/v3/__init__.py
+++ uniswap/v3/__init__.py
@@ -1,3 +1,6 @@
 from . import functions, libraries
 from .tick_lens import TickLens
 from .v3_liquidity_pool import V3LiquidityPool
+
+# alias for older scripts that may be floating around
+import degenbot.uniswap.abi as abi
