e64796c117faaddf1b9529ba699900a722a480c7
BowTiedDevil <devil@bowtieddevil.com>
2023-06-15 17:13:59 -0700

    remove ABC import

--- uniswap/v3/v3_liquidity_pool.py
+++ uniswap/v3/v3_liquidity_pool.py
@@ -1,4 +1,4 @@
-from abc import ABC, abstractmethod
+from abc import abstractmethod
 from decimal import Decimal
 from threading import Lock
 from typing import Any, Dict, List, Optional, Tuple, Union
