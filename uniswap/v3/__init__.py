3fb4394ce16c5c4294de679a4e3cb51bcaac83e4
BowTiedDevil <devil@bowtieddevil.com>
2023-06-22 00:10:30 -0700

    sort imports

--- uniswap/v3/__init__.py
+++ uniswap/v3/__init__.py
@@ -1,6 +1,6 @@
+import degenbot.uniswap.abi as abi  # alias for older scripts that may be floating around
+
 from . import functions, libraries
+from .snapshot import V3LiquiditySnapshot
 from .tick_lens import TickLens
 from .v3_liquidity_pool import V3LiquidityPool
-
-# alias for older scripts that may be floating around
-import degenbot.uniswap.abi as abi
