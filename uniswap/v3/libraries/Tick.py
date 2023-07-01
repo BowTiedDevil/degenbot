c238635ae141a48fe185f9f2b33f919a66d0afff
BowTiedDevil <devil@bowtieddevil.com>
2023-06-30 23:46:12 -0700

    Clean up old asserts

--- uniswap/v3/libraries/Tick.py
+++ uniswap/v3/libraries/Tick.py
@@ -4,14 +4,11 @@ from decimal import Decimal
 
 # type hinting aliases
 Int24 = int
-Uint24 = int
 
 
 def tickSpacingToMaxLiquidityPerTick(tickSpacing: Int24):
-
     if not (-(2**23) <= tickSpacing <= 2**23 - 1):
         raise EVMRevertError("input not a valid int24")
-    # assert -(2**23) <= tickSpacing <= 2**23 - 1, "input not a valid int24"
 
     minTick = Decimal(TickMath.MIN_TICK) // tickSpacing * tickSpacing
     maxTick = Decimal(TickMath.MAX_TICK) // tickSpacing * tickSpacing
@@ -21,6 +18,5 @@ def tickSpacingToMaxLiquidityPerTick(tickSpacing: Int24):
 
     if not (0 <= result <= MAX_UINT128):
         raise EVMRevertError("result not a valid uint128")
-    # assert 0 <= result <= MAX_UINT128, "result not a valid uint128"
 
     return result
