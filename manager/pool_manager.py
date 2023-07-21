cabed31d47eb8cd97b663a6ec183704c620ccbc3
BowTiedDevil <devil@bowtieddevil.com>
2023-07-21 11:07:44 -0700

    Remove checksum function, use web3 method directly

--- manager/pool_manager.py
+++ manager/pool_manager.py
@@ -15,20 +15,6 @@ _all_pools: Dict[
 ] = {}
 
 
-def to_checksum_address(address: Union[ChecksumAddress, str]):
-    """
-    Return the checksummed version of an address.
-    """
-    if type(address) == ChecksumAddress:
-        return address
-    elif isinstance(address, str):
-        return Web3.toChecksumAddress(address)
-    else:
-        raise ValueError(
-            f"Unexpected input. Expected str or ChecksumAddress, got {type(address)}"
-        )
-
-
 class AllPools:
     def __init__(self, chain_id):
         try:
@@ -39,11 +25,11 @@ class AllPools:
             self.pools = _all_pools[chain_id]
 
     def __delitem__(self, pool_address: Union[ChecksumAddress, str]):
-        _pool_address = to_checksum_address(pool_address)
+        _pool_address = Web3.toChecksumAddress(pool_address)
         del self.pools[_pool_address]
 
     def __getitem__(self, pool_address: Union[ChecksumAddress, str]):
-        _pool_address = to_checksum_address(pool_address)
+        _pool_address = Web3.toChecksumAddress(pool_address)
         return self.pools[_pool_address]
 
     def __setitem__(
@@ -51,7 +37,7 @@ class AllPools:
         pool_address: Union[ChecksumAddress, str],
         pool_helper: PoolHelper,
     ):
-        _pool_address = to_checksum_address(pool_address)
+        _pool_address = Web3.toChecksumAddress(pool_address)
 
         if self.pools.get(_pool_address):
             raise PoolAlreadyExistsError(
@@ -64,5 +50,5 @@ class AllPools:
         return len(self.pools)
 
     def get(self, pool_address: Union[ChecksumAddress, str]):
-        _pool_address = to_checksum_address(pool_address)
+        _pool_address = Web3.toChecksumAddress(pool_address)
         return self.pools.get(_pool_address)
