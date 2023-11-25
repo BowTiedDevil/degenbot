418bb14fb2f82d235f12ab4ea1eba63ec81bfa77
BowTiedDevil <devil@bowtieddevil.com>
2023-11-24 16:03:25 -0800

    Provide a default Web3 connection on import

--- src/degenbot/config.py
+++ src/degenbot/config.py
@@ -1,11 +1,14 @@
+import sys
 from typing import Callable, Optional
 
 from web3 import Web3
 
-_web3: Optional[Web3] = None
+from .logging import logger
+
+_web3: Web3
 
 
-def get_web3() -> Optional[Web3]:
+def get_web3() -> Web3:
     return _web3
 
 
@@ -26,5 +29,24 @@ def set_web3(w3: Web3):
     if connected_method() is False:  # pragma: no cover
         raise ValueError("Web3 object is not connected.")
 
+    logger.info(f"Connected to Web3 provider {w3.provider}")
+
     global _web3
     _web3 = w3
+
+
+if "brownie" in sys.modules:  # pragma: no cover
+    logger.info("Brownie detected. Degenbot will attempt to use its Web3 object...")
+    from brownie import web3 as brownie_web3  # type: ignore[import]
+
+    set_web3(brownie_web3)
+
+else:
+    logger.info("Attempting to use Web3 AutoProvider")
+    try:
+        set_web3(Web3())
+    except Exception as e:
+        logger.error(e)
+        logger.info(
+            "Could not establish Web3 connection using AutoProvider. Provide a Web3 instance to set_web3() before use"
+        )
