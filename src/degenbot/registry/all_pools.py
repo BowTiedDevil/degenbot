a81cf6bc7aa41cf4f027c69dabbe06d8cae4a2ff
BowTiedDevil <devil@bowtieddevil.com>
2024-04-14 15:34:43 -0700

    Refactor and add tests for token/pool registries

--- src/degenbot/registry/all_pools.py
+++ src/degenbot/registry/all_pools.py
@@ -6,9 +6,8 @@ from eth_utils.address import to_checksum_address
 from ..baseclasses import BaseLiquidityPool
 from ..logging import logger
 
-# Internal state dictionary that maintains a keyed dictionary of all
-# pool helper objects. The top level dict is keyed by chain ID, and
-# sub-dicts are keyed by the checksummed pool address.
+# Internal state dictionary that maintains a keyed dictionary of all pool objects. The top level
+# dict is keyed by chain ID, and sub-dicts are keyed by the checksummed pool address.
 _all_pools: Dict[
     int,
     Dict[ChecksumAddress, BaseLiquidityPool],
@@ -24,6 +23,13 @@ class AllPools:
         finally:
             self.pools = _all_pools[chain_id]
 
+    def __contains__(self, pool: BaseLiquidityPool | str) -> bool:
+        if isinstance(pool, BaseLiquidityPool):
+            _pool_address = pool.address
+        else:
+            _pool_address = to_checksum_address(pool)
+        return _pool_address in self.pools
+
     def __delitem__(self, pool: BaseLiquidityPool | str) -> None:
         if isinstance(pool, BaseLiquidityPool):
             _pool_address = pool.address
@@ -36,13 +42,13 @@ class AllPools:
 
     def __setitem__(self, pool_address: str, pool_helper: BaseLiquidityPool) -> None:
         _pool_address = to_checksum_address(pool_address)
-        if self.pools.get(_pool_address):
+        if _pool_address in self.pools:  # pragma: no cover
             logger.warning(
-                f"A pool helper with address {_pool_address} already exists! It has been overwritten."
+                f"Pool with address {_pool_address} already known. It has been overwritten."
             )
         self.pools[_pool_address] = pool_helper
 
-    def __len__(self) -> int:
+    def __len__(self) -> int:  # pragma: no cover
         return len(self.pools)
 
     def get(self, pool_address: str) -> BaseLiquidityPool | None:
--- src/degenbot/registry/all_tokens.py
+++ src/degenbot/registry/all_tokens.py
@@ -1,10 +1,13 @@
 from typing import Dict
-from eth_utils.address import to_checksum_address
+
 from eth_typing import ChecksumAddress
+from eth_utils.address import to_checksum_address
 
 from ..baseclasses import BaseToken
+from ..logging import logger
 
-
+# Internal state dictionary that maintains a keyed dictionary of all token objects. The top level
+# dict is keyed by chain ID, and sub-dicts are keyed by the checksummed token address.
 _all_tokens: Dict[
     int,
     Dict[ChecksumAddress, BaseToken],
@@ -20,16 +23,32 @@ class AllTokens:
         finally:
             self.tokens = _all_tokens[chain_id]
 
-    def __delitem__(self, token_address: str) -> None:
-        del self.tokens[to_checksum_address(token_address)]
+    def __contains__(self, token: BaseToken | str) -> bool:
+        if isinstance(token, BaseToken):
+            _token_address = token.address
+        else:
+            _token_address = to_checksum_address(token)
+        return _token_address in self.tokens
+
+    def __delitem__(self, token: BaseToken | str) -> None:
+        if isinstance(token, BaseToken):
+            _token_address = token.address
+        else:
+            _token_address = to_checksum_address(token)
+        del self.tokens[_token_address]
 
     def __getitem__(self, token_address: str) -> BaseToken:
         return self.tokens[to_checksum_address(token_address)]
 
     def __setitem__(self, token_address: str, token_helper: BaseToken) -> None:
+        _token_address = to_checksum_address(token_address)
+        if _token_address in self.tokens:  # pragma: no cover
+            logger.warning(
+                f"Token with address {_token_address} already known. It has been overwritten."
+            )
         self.tokens[to_checksum_address(token_address)] = token_helper
 
-    def __len__(self) -> int:
+    def __len__(self) -> int:  # pragma: no cover
         return len(self.tokens)
 
     def get(self, token_address: str) -> BaseToken | None:
