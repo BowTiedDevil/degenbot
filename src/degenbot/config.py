b0cd9589085f09673c6979c76c8b425b3ca6603c
BowTiedDevil <devil@bowtieddevil.com>
2024-06-14 00:11:54 -0700

    WIP: Refactor web3 getter/setter

--- src/degenbot/config.py
+++ src/degenbot/config.py
@@ -1,23 +1,47 @@
-from web3 import Web3
+import web3
+import web3.providers
 
 from .exceptions import DegenbotError
 from .logging import logger
 
-_web3: Web3
+_web3: web3.Web3
+_provider_address: str
+_ipc_path: str
 
 
-def get_web3() -> Web3:
+def get_web3() -> web3.Web3:
+    global _web3
+
     try:
         return _web3
     except NameError:
-        raise DegenbotError("A Web3 instance has not been provided.") from None
+        try:
+            _provider_address
+        except NameError:
+            _web3 = web3.Web3(web3.IPCProvider(_ipc_path))
+            return _web3
+        else:
+            if "http://" in _provider_address or "https://" in _provider_address:
+                _web3 = web3.Web3(web3.HTTPProvider(_provider_address))
+                return _web3
+            elif "ws://" in _provider_address or "wss://" in _provider_address:
+                _web3 = web3.Web3(web3.WebsocketProvider(_provider_address))
+                return _web3
+            raise DegenbotError("A Web3 instance has not been provided.") from None
 
 
-def set_web3(w3: Web3) -> None:
+def set_web3(w3: web3.Web3) -> None:
     if w3.is_connected() is False:
         raise DegenbotError("Web3 object is not connected.")
 
     logger.info(f"Connected to Web3 provider {w3.provider}")
 
     global _web3
+    global _provider_address
+    global _ipc_path
+
     _web3 = w3
+    if isinstance(w3.provider, (web3.HTTPProvider, web3.WebsocketProvider)):
+        _provider_address = w3.provider.endpoint_uri
+    if isinstance(w3.provider, web3.IPCProvider):
+        _ipc_path = w3.provider.ipc_path
