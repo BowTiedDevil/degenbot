5836eb3313672f0fbf6a1a3b45d750e9d501ec8a
BowTiedDevil <devil@bowtieddevil.com>
2024-09-07 17:02:42 -0700

    Clean up namespace, refactor shared web3 method to support multiple chains

--- src/degenbot/config.py
+++ src/degenbot/config.py
@@ -1,50 +1,42 @@
+__all__ = (
+    "get_web3",
+    "set_web3",
+    "web3_connection_manager",
+)
+
 import web3
-import web3.providers
+from eth_typing import ChainId
 
 from .exceptions import DegenbotError
-from .logging import logger
-
-_web3: web3.Web3
-_endpoint_uri: str
-_ipc_path: str
 
 
-def get_web3() -> web3.Web3:
-    global _web3
+class Web3ConnectionManager:
+    def __init__(self) -> None:
+        self.connections: dict[int, web3.Web3] = dict()
+        self._default_chain_id: int = ChainId.ETH
 
-    try:
-        return _web3
-    except NameError:
-        try:
-            _ = _endpoint_uri
-        except NameError:
-            _web3 = web3.Web3(web3.IPCProvider(_ipc_path))
-            return _web3
-        else:
-            if "http://" in _endpoint_uri or "https://" in _endpoint_uri:
-                _web3 = web3.Web3(web3.HTTPProvider(_endpoint_uri))
-                return _web3
-            elif "ws://" in _endpoint_uri or "wss://" in _endpoint_uri:
-                _web3 = web3.Web3(web3.WebsocketProvider(_endpoint_uri))
-                return _web3
-            raise DegenbotError("A Web3 instance has not been provided.") from None
+    def register(self, w3: web3.Web3) -> None:
+        if not self.connections:
+            self._default_chain_id = w3.eth.chain_id
+        self.connections[w3.eth.chain_id] = w3
+
+    def get(self, chain_id: int) -> web3.Web3 | None:
+        return self.connections.get(chain_id)
+
+
+def get_web3(chain_id: int | None = None) -> web3.Web3:
+    if chain_id is None:
+        chain_id = web3_connection_manager._default_chain_id
+    if (w3 := web3_connection_manager.get(chain_id)) is not None:
+        return w3
+    raise DegenbotError(f"No Web3 instance has not been registered for chain ID {chain_id}.")
 
 
 def set_web3(w3: web3.Web3) -> None:
     if w3.is_connected() is False:
         raise DegenbotError("Web3 object is not connected.")
 
-    logger.info(f"Connected to Web3 provider {w3.provider}")
+    web3_connection_manager.register(w3)
 
-    global _web3
-    global _endpoint_uri
-    global _ipc_path
 
-    _web3 = w3
-    match w3.provider:
-        case web3.HTTPProvider() | web3.WebsocketProvider():
-            _endpoint_uri = w3.provider.endpoint_uri  # type: ignore[assignment]
-        case web3.IPCProvider():
-            _ipc_path = w3.provider.ipc_path  # type: ignore[has-type]
-        case _:
-            raise TypeError("Unsupported provider.")
+web3_connection_manager = Web3ConnectionManager()
