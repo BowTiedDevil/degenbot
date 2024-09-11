fa8092d946c6a0736a523a149ec8b78c0283fba5
BowTiedDevil <devil@bowtieddevil.com>
2024-09-10 23:51:55 -0700

    Add calc method, implement Solidly volatile calc

--- src/degenbot/solidly/solidly_liquidity_pool.py
+++ src/degenbot/solidly/solidly_liquidity_pool.py
@@ -5,11 +5,9 @@ from typing import Any, Literal
 
 from eth_typing import ChecksumAddress
 from eth_utils.address import to_checksum_address
+from eth_utils.crypto import keccak
 from typing_extensions import override
 from web3.contract.contract import Contract
-
-from degenbot.exchanges.solidly.deployments import FACTORY_DEPLOYMENTS
-from degenbot.exchanges.solidly.types import SolidlyExchangeDeployment
 
 from .. import config
 from ..erc20_token import Erc20Token
@@ -19,19 +17,26 @@ from ..exceptions import (
     NoPoolStateAvailable,
     ZeroSwapError,
 )
+from ..exchanges.solidly.deployments import FACTORY_DEPLOYMENTS
+from ..exchanges.solidly.types import SolidlyExchangeDeployment
 from ..logging import logger
 from ..manager.token_manager import Erc20TokenHelperManager
 from ..registry.all_pools import AllPools
 from ..types import AbstractLiquidityPool
-from ..uniswap.v2_functions import (
-    constant_product_calc_exact_in,
-    constant_product_calc_exact_out,
+from .abi import AERODROME_V2_FACTORY_ABI, AERODROME_V2_POOL_ABI
+from .solidly_functions import (
+    generate_aerodrome_v2_pool_address,
+    solidly_calc_exact_in_stable,
+    solidly_calc_exact_in_volatile,
 )
-from .abi import AERODROME_V2_POOL_ABI
-from .solidly_functions import generate_aerodrome_v2_pool_address
+from .types import (
+    AerodromeV2PoolSimulationResult,
+    AerodromeV2PoolState,
+    AerodromeV2PoolStateUpdated,
+)
 
 
-class SolidlyV2LiquidityPool(AbstractLiquidityPool):
+class AerodromeV2LiquidityPool(AbstractLiquidityPool):
     def __init__(
         self,
         address: ChecksumAddress | str,
@@ -40,17 +45,18 @@ class SolidlyV2LiquidityPool(AbstractLiquidityPool):
         factory_address: str | None = None,
         deployer_address: str | None = None,
         init_hash: str | None = None,
+        fee: Fraction | None = None,
         silent: bool = False,
+        archive_states: bool = True,
         verify_address: bool = True,
     ) -> None:
         self.address = to_checksum_address(address)
-
-        self.address: ChecksumAddress = to_checksum_address(address)
         self.abi = abi if abi is not None else AERODROME_V2_POOL_ABI
 
         w3 = config.get_web3()
-        w3_contract = self.w3_contract
+        self.update_block = w3.eth.block_number
         chain_id = w3.eth.chain_id
+        w3_contract = self.w3_contract
 
         self.factory = (
             to_checksum_address(factory_address)
@@ -62,13 +68,18 @@ class SolidlyV2LiquidityPool(AbstractLiquidityPool):
         )
         self.stable = w3_contract.functions.stable().call()
 
-        if init_hash is not None:
+        try:
+            self.init_hash = FACTORY_DEPLOYMENTS[chain_id][self.factory].pool_init_hash
+        except KeyError:
             self.init_hash = init_hash
-        else:
-            try:
-                self.init_hash = FACTORY_DEPLOYMENTS[chain_id][self.factory].pool_init_hash
-            except KeyError:
-                self.init_hash = UNISWAP_V2_MAINNET_POOL_INIT_HASH
+
+        if fee is None:
+            factory_contract = w3.eth.contract(address=self.factory, abi=AERODROME_V2_FACTORY_ABI)
+            pool_fee = factory_contract.functions.getFee(self.address, self.stable).call()
+            print(f"{fee=}")
+            fee = Fraction(pool_fee, 10_000)
+
+        self.fee = fee
 
         if tokens is not None:
             self.token0, self.token1 = sorted(tokens)
@@ -82,7 +93,6 @@ class SolidlyV2LiquidityPool(AbstractLiquidityPool):
                 address=w3_contract.functions.token1().call(),
                 silent=silent,
             )
-
         self.tokens = (self.token0, self.token1)
 
         if verify_address:
@@ -92,16 +102,101 @@ class SolidlyV2LiquidityPool(AbstractLiquidityPool):
                     f"Pool address verification failed. Provided: {self.address}, expected: {verified_address}"  # noqa:E501
                 )
 
+        fee_string = f"{100*self.fee.numerator/self.fee.denominator:.2f}"
+        self.name = f"{self.token0}-{self.token1} (V2, {fee_string}%)"
+
+        self.reserves_token0, self.reserves_token1, *_ = (
+            self.w3_contract.functions.getReserves().call(block_identifier=self.update_block)
+        )
+        self._state = AerodromeV2PoolState(
+            pool=self.address,
+            reserves_token0=self.reserves_token0,
+            reserves_token1=self.reserves_token1,
+        )
+        self._pool_state_archive = {self.update_block: self._state} if archive_states else None
+
+        AllPools(chain_id)[self.address] = self
+
+        self._subscribers = set()
+
+        if not silent:  # pragma: no cover
+            logger.info(self.name)
+            logger.info(f"• Token 0: {self.token0} - Reserves: {self.reserves_token0}")
+            logger.info(f"• Token 1: {self.token1} - Reserves: {self.reserves_token1}")
+
     def _verified_address(self) -> ChecksumAddress:
+        # The implementation address is hard-coded into the contract
+        implementation_address = to_checksum_address(
+            config.get_web3().eth.get_code(self.address)[10:30]
+        )
+
         return generate_aerodrome_v2_pool_address(
             deployer_address=self.deployer_address,
             token_addresses=(self.token0.address, self.token1.address),
-            implementation_address=to_checksum_address(
-                "0xA4e46b4f701c62e14DF11B48dCe76A7d793CD6d7"
-            ),
+            implementation_address=to_checksum_address(implementation_address),
             stable=self.stable,
         )
 
     @property
     def w3_contract(self) -> Contract:
         return config.get_web3().eth.contract(address=self.address, abi=self.abi)
+
+    def calculate_tokens_out_from_tokens_in(
+        self,
+        token_in: Erc20Token,
+        token_in_quantity: int,
+        override_state: AerodromeV2PoolState | None = None,
+    ) -> int:
+        """
+        Calculates the expected token OUTPUT for a target INPUT at current pool reserves.
+        """
+
+        if token_in_quantity <= 0:  # pragma: no cover
+            raise ZeroSwapError("token_in_quantity must be positive")
+
+        if override_state:  # pragma: no cover
+            logger.debug(f"State overrides applied: {override_state}")
+
+        if token_in == self.token0:
+            reserves_in = (
+                override_state.reserves_token0
+                if override_state is not None
+                else self.reserves_token0
+            )
+            reserves_out = (
+                override_state.reserves_token1
+                if override_state is not None
+                else self.reserves_token1
+            )
+        elif token_in == self.token1:
+            reserves_in = (
+                override_state.reserves_token1
+                if override_state is not None
+                else self.reserves_token1
+            )
+            reserves_out = (
+                override_state.reserves_token0
+                if override_state is not None
+                else self.reserves_token0
+            )
+        else:  # pragma: no cover
+            raise ValueError(
+                f"Could not identify token_in: {token_in}! Pool holds: {self.token0} {self.token1}"
+            )
+
+        if self.stable:
+            return solidly_calc_exact_in_stable(
+                amount_in=token_in_quantity,
+                token_in=...,
+                reserves0=self.reserves_token0,
+                reserves1=self.reserves_token1,
+                fee=self.fee,
+            )
+        else:
+            return solidly_calc_exact_in_volatile(
+                amount_in=token_in_quantity,
+                token_in=int(token_in == self.token1),
+                reserves0=self.reserves_token0,
+                reserves1=self.reserves_token1,
+                fee=self.fee,
+            )
