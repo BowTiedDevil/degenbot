c434d74e4241d206371804b473f630a394091a21
BowTiedDevil <devil@bowtieddevil.com>
2024-09-25 12:14:34 -0700

    Convert to match-case

--- src/degenbot/types.py
+++ src/degenbot/types.py
@@ -4,6 +4,7 @@ from collections.abc import Sequence
 from typing import TYPE_CHECKING, Any, Protocol
 
 from eth_typing import ChecksumAddress
+from hexbytes import HexBytes
 
 if TYPE_CHECKING:
     from .erc20_token import Erc20Token
@@ -91,34 +92,43 @@ class AbstractLiquidityPool(abc.ABC, Publisher):
     _subscribers: set[Subscriber]
 
     def __eq__(self, other: Any) -> bool:
-        if isinstance(other, AbstractLiquidityPool):
-            return self.address == other.address
-        elif isinstance(other, bytes):
-            return self.address.lower() == other.hex().lower()
-        elif isinstance(other, str):
-            return self.address.lower() == other.lower()
-        else:
-            return NotImplemented
+        match other:
+            case AbstractLiquidityPool():
+                return self.address == other.address
+            case HexBytes():
+                return self.address.lower() == other.to_0x_hex().lower()
+            case bytes():
+                return self.address.lower() == "0x" + other.hex().lower()
+            case str():
+                return self.address.lower() == other.lower()
+            case _:
+                return NotImplemented
 
     def __lt__(self, other: Any) -> bool:
-        if isinstance(other, AbstractLiquidityPool):
-            return self.address < other.address
-        elif isinstance(other, bytes):
-            return self.address.lower() < other.hex().lower()
-        elif isinstance(other, str):
-            return self.address.lower() < other.lower()
-        else:
-            return NotImplemented
+        match other:
+            case AbstractLiquidityPool():
+                return self.address < other.address
+            case HexBytes():
+                return self.address.lower() < other.to_0x_hex().lower()
+            case bytes():
+                return self.address.lower() < "0x" + other.hex().lower()
+            case str():
+                return self.address.lower() < other.lower()
+            case _:
+                return NotImplemented
 
     def __gt__(self, other: Any) -> bool:
-        if isinstance(other, AbstractLiquidityPool):
-            return self.address > other.address
-        elif isinstance(other, bytes):
-            return self.address.lower() > other.hex().lower()
-        elif isinstance(other, str):
-            return self.address.lower() > other.lower()
-        else:
-            return NotImplemented
+        match other:
+            case AbstractLiquidityPool():
+                return self.address > other.address
+            case HexBytes():
+                return self.address.lower() > other.to_0x_hex().lower()
+            case bytes():
+                return self.address.lower() > "0x" + other.hex().lower()
+            case str():
+                return self.address.lower() > other.lower()
+            case _:
+                return NotImplemented
 
     def __hash__(self) -> int:
         return hash(self.address)
@@ -151,34 +161,43 @@ class AbstractErc20Token:
     decimals: int
 
     def __eq__(self, other: Any) -> bool:
-        if isinstance(other, AbstractErc20Token):
-            return self.address == other.address
-        elif isinstance(other, bytes):
-            return self.address.lower() == other.hex().lower()
-        elif isinstance(other, str):
-            return self.address.lower() == other.lower()
-        else:
-            return NotImplemented
+        match other:
+            case AbstractErc20Token():
+                return self.address == other.address
+            case HexBytes():
+                return self.address.lower() == other.to_0x_hex().lower()
+            case bytes():
+                return self.address.lower() == "0x" + other.hex().lower()
+            case str():
+                return self.address.lower() == other.lower()
+            case _:
+                return NotImplemented
 
     def __lt__(self, other: Any) -> bool:
-        if isinstance(other, AbstractErc20Token):
-            return self.address < other.address
-        elif isinstance(other, bytes):
-            return self.address.lower() < other.hex().lower()
-        elif isinstance(other, str):
-            return self.address.lower() < other.lower()
-        else:
-            return NotImplemented
+        match other:
+            case AbstractErc20Token():
+                return self.address == other.address
+            case HexBytes():
+                return self.address.lower() < other.to_0x_hex().lower()
+            case bytes():
+                return self.address.lower() < "0x" + other.hex().lower()
+            case str():
+                return self.address.lower() < other.lower()
+            case _:
+                return NotImplemented
 
     def __gt__(self, other: Any) -> bool:
-        if isinstance(other, AbstractErc20Token):
-            return self.address > other.address
-        elif isinstance(other, bytes):
-            return self.address.lower() > other.hex().lower()
-        elif isinstance(other, str):
-            return self.address.lower() > other.lower()
-        else:
-            return NotImplemented
+        match other:
+            case AbstractErc20Token():
+                return self.address == other.address
+            case HexBytes():
+                return self.address.lower() > other.to_0x_hex().lower()
+            case bytes():
+                return self.address.lower() > "0x" + other.hex().lower()
+            case str():
+                return self.address.lower() > other.lower()
+            case _:
+                return NotImplemented
 
     def __hash__(self) -> int:
         return hash(self.address)
