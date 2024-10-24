2654ec9cd73069e14a38b9a1834fa8164cc86043
BowTiedDevil <devil@bowtieddevil.com>
2024-10-24 15:23:18 -0700

    Refactor `get_d`

--- src/degenbot/curve/curve_stableswap_liquidity_pool.py
+++ src/degenbot/curve/curve_stableswap_liquidity_pool.py
@@ -54,12 +54,60 @@ from .types import (
 class CurveStableswapPool(AbstractLiquidityPool):
     # Constants from contract
     # ref: https://github.com/curvefi/curve-contract/blob/master/contracts/pool-templates/base/SwapTemplateBase.vy
-    PRECISION_DECIMALS = 18
-    PRECISION = 10**PRECISION_DECIMALS
-    LENDING_PRECISION = PRECISION
-    FEE_DENOMINATOR = 10**10
-    A_PRECISION = 100
-    MAX_COINS = 8
+    PRECISION_DECIMALS: int = 18
+    PRECISION: int = 10**PRECISION_DECIMALS
+    LENDING_PRECISION: int = PRECISION
+    FEE_DENOMINATOR: int = 10**10
+    A_PRECISION: int = 100
+    MAX_COINS: int = 8
+
+    D_VARIANT_GROUP_0 = frozenset(
+        (
+            "0x06364f10B501e868329afBc005b3492902d6C763",
+            "0x4CA9b3063Ec5866A4B82E437059D2C43d1be596F",
+            "0x52EA46506B9CC5Ef470C5bf89f17Dc28bB35D85C",
+            "0x7fC77b5c7614E1533320Ea6DDc2Eb61fa00A9714",
+            "0x93054188d876f558f4a66B2EF1d97d16eDf0895B",
+            "0xbEbc44782C7dB0a1A60Cb6fe97d0b483032FF1C7",
+        )
+    )
+    D_VARIANT_GROUP_1 = frozenset(
+        (
+            "0x45F783CCE6B7FF23B2ab2D70e416cdb7D6055f51",
+            "0x79a8C46DeA5aDa233ABaFFD40F3A0A2B1e5A4F27",
+            "0xA2B47E3D5c44877cca798226B7B8118F9BFb7A56",
+            "0xA5407eAE9Ba41422680e2e00537571bcC53efBfD",
+        )
+    )
+    D_VARIANT_GROUP_2 = frozenset(
+        (
+            "0x0AD66FeC8dB84F8A3365ADA04aB23ce607ac6E24",
+            "0x1C5F80b6B68A9E1Ef25926EeE00b5255791b996B",
+            "0x1c899dED01954d0959E034b62a728e7fEbE593b0",
+            "0x26f3f26F46cBeE59d1F8860865e13Aa39e36A8c0",
+            "0x320B564Fb9CF36933eC507a846ce230008631fd3",
+            "0x3b21C2868B6028CfB38Ff86127eF22E68d16d53B",
+            "0x3F1B0278A9ee595635B61817630cC19DE792f506",
+            "0x3Fb78e61784C9c637D560eDE23Ad57CA1294c14a",
+            "0x453D92C7d4263201C69aACfaf589Ed14202d83a4",
+            "0x663aC72a1c3E1C4186CD3dCb184f216291F4878C",
+            "0x69ACcb968B19a53790f43e57558F5E443A91aF22",
+            "0x6A274dE3e2462c7614702474D64d376729831dCa",
+            "0x875DF0bA24ccD867f8217593ee27253280772A97",
+            "0x971add32Ea87f10bD192671630be3BE8A11b8623",
+            "0x9D0464996170c6B9e75eED71c68B99dDEDf279e8",
+            "0xB657B895B265C38c53FFF00166cF7F6A3C70587d",
+            "0xCA0253A98D16e9C1e3614caFDA19318EE69772D0",
+            "0xD6Ac1CB9019137a896343Da59dDE6d097F710538",
+        )
+    )
+    D_VARIANT_GROUP_3 = frozenset(
+        (
+            "0xDC24316b9AE028F1497c275EB9192a3Ea0f67022",
+            "0xDeBF20617708857ebe4F679508E7b7863a8A8EeE",
+            "0xEB16Ae0052ed37f479f7fe63849198Df1765a733",
+        )
+    )
 
     def __init__(
         self,
@@ -1658,128 +1706,97 @@ class CurveStableswapPool(AbstractLiquidityPool):
         return admin_balance
 
     def _get_d(self, _xp: Sequence[int], _amp: int) -> int:
-        n_coins = len(self.tokens)
+        """
+        Solve for the Curve stableswap invariant D, using a modified Newton's method.
 
-        s = sum(_xp)
+        Mainnet V1 Curve pools have several calculation variants to calculate the D and D_prev
+        values. The pool addresses using each variant are grouped and the appropriate function is
+        set at runtime.
+        """
+
+        def calc_d(
+            *,
+            a_nn: int,
+            s: int,
+            d: int,
+            d_p: int,
+            n_coins: int,
+            a_precision: int,
+        ) -> int:
+            return (
+                (a_nn * s // a_precision + d_p * n_coins)
+                * d
+                // ((a_nn - a_precision) * d // a_precision + (n_coins + 1) * d_p)
+            )
+
+        def calc_d_variant_alpha(
+            *,
+            a_nn: int,
+            s: int,
+            d: int,
+            d_p: int,
+            n_coins: int,
+            a_precision: int,  # noqa:ARG001
+        ) -> int:
+            return (a_nn * s + d_p * n_coins) * d // ((a_nn - 1) * d + (n_coins + 1) * d_p)
+
+        def calc_dp(
+            *,
+            d: int,
+            d_p: int,
+            xp: Sequence[int],
+        ) -> int:
+            for x in xp:
+                d_p = d_p * d // (x * n_coins)
+            return d_p
+
+        def calc_dp_variant_alpha(
+            *,
+            d: int,
+            d_p: int,
+            xp: Sequence[int],
+        ) -> int:
+            for x in xp:
+                d_p = d_p * d // (x * n_coins + 1)
+            return d_p
+
+        def calc_dp_variant_beta(
+            *,
+            d: int,
+            d_p: int,  # noqa:ARG001
+            xp: Sequence[int],
+        ) -> int:
+            return d * d // xp[0] * d // xp[1] // n_coins**2
+
+        d_func = calc_d
+        dp_func = calc_dp
+        if self.address in self.D_VARIANT_GROUP_0:
+            d_func = calc_d_variant_alpha
+        elif self.address in self.D_VARIANT_GROUP_1:
+            d_func = calc_d_variant_alpha
+            dp_func = calc_dp_variant_alpha
+        elif self.address in self.D_VARIANT_GROUP_2:
+            dp_func = calc_dp_variant_beta
+        elif self.address in self.D_VARIANT_GROUP_3:
+            dp_func = calc_dp_variant_alpha
+
+        d = s = sum(_xp)
         if s == 0:
             return 0
-
-        d = s
+        n_coins = len(self.tokens)
         a_nn = _amp * n_coins
 
-        if self.address in (
-            "0x06364f10B501e868329afBc005b3492902d6C763",
-            "0x4CA9b3063Ec5866A4B82E437059D2C43d1be596F",
-            "0x52EA46506B9CC5Ef470C5bf89f17Dc28bB35D85C",
-            "0x7fC77b5c7614E1533320Ea6DDc2Eb61fa00A9714",
-            "0x93054188d876f558f4a66B2EF1d97d16eDf0895B",
-            "0xbEbc44782C7dB0a1A60Cb6fe97d0b483032FF1C7",
-        ):
-            for _ in range(255):
-                d_p = d
-                for _x in _xp:
-                    d_p = d_p * d // (_x * n_coins)
-                d_prev = d
-                d = (a_nn * s + d_p * n_coins) * d // ((a_nn - 1) * d + (n_coins + 1) * d_p)
-                if d_prev < d:
-                    if d - d_prev <= 1:
-                        return d
-                elif d_prev - d <= 1:
-                    return d
-
-        elif self.address in (
-            "0x45F783CCE6B7FF23B2ab2D70e416cdb7D6055f51",
-            "0x79a8C46DeA5aDa233ABaFFD40F3A0A2B1e5A4F27",
-            "0xA2B47E3D5c44877cca798226B7B8118F9BFb7A56",
-            "0xA5407eAE9Ba41422680e2e00537571bcC53efBfD",
-        ):
-            for _ in range(255):
-                d_p = d
-                for _x in _xp:
-                    d_p = d_p * d // (_x * n_coins + 1)
-                d_prev = d
-                d = (a_nn * s + d_p * n_coins) * d // ((a_nn - 1) * d + (n_coins + 1) * d_p)
-                if d_prev < d:
-                    if d - d_prev <= 1:
-                        return d
-                elif d_prev - d <= 1:
-                    return d
-
-        elif self.address in (
-            "0x0AD66FeC8dB84F8A3365ADA04aB23ce607ac6E24",
-            "0x1C5F80b6B68A9E1Ef25926EeE00b5255791b996B",
-            "0x1c899dED01954d0959E034b62a728e7fEbE593b0",
-            "0x26f3f26F46cBeE59d1F8860865e13Aa39e36A8c0",
-            "0x320B564Fb9CF36933eC507a846ce230008631fd3",
-            "0x3b21C2868B6028CfB38Ff86127eF22E68d16d53B",
-            "0x3F1B0278A9ee595635B61817630cC19DE792f506",
-            "0x3Fb78e61784C9c637D560eDE23Ad57CA1294c14a",
-            "0x453D92C7d4263201C69aACfaf589Ed14202d83a4",
-            "0x69ACcb968B19a53790f43e57558F5E443A91aF22",
-            "0x6A274dE3e2462c7614702474D64d376729831dCa",
-            "0x875DF0bA24ccD867f8217593ee27253280772A97",
-            "0x971add32Ea87f10bD192671630be3BE8A11b8623",
-            "0x9D0464996170c6B9e75eED71c68B99dDEDf279e8",
-            "0xB657B895B265C38c53FFF00166cF7F6A3C70587d",
-            "0xCA0253A98D16e9C1e3614caFDA19318EE69772D0",
-            "0xD6Ac1CB9019137a896343Da59dDE6d097F710538",
-            "0x663aC72a1c3E1C4186CD3dCb184f216291F4878C",
-        ):
-            for _ in range(255):
-                d_p = d * d // _xp[0] * d // _xp[1] // n_coins**2
-                d_prev = d
-                d = (
-                    (a_nn * s // self.A_PRECISION + d_p * n_coins)
-                    * d
-                    // ((a_nn - self.A_PRECISION) * d // self.A_PRECISION + (n_coins + 1) * d_p)
-                )
-                if d_prev < d:
-                    if d - d_prev <= 1:
-                        return d
-                elif d_prev - d <= 1:
-                    return d
-
-        elif self.address in (
-            "0xDC24316b9AE028F1497c275EB9192a3Ea0f67022",
-            "0xDeBF20617708857ebe4F679508E7b7863a8A8EeE",
-            "0xEB16Ae0052ed37f479f7fe63849198Df1765a733",
-        ):
-            for _ in range(255):
-                d_p = d
-                for _x in _xp:
-                    d_p = d_p * d // (_x * n_coins + 1)
-                d_prev = d
-                d = (
-                    (a_nn * s // self.A_PRECISION + d_p * n_coins)
-                    * d
-                    // ((a_nn - self.A_PRECISION) * d // self.A_PRECISION + (n_coins + 1) * d_p)
-                )
-                if d_prev < d:
-                    if d - d_prev <= 1:
-                        return d
-                elif d_prev - d <= 1:
-                    return d
-
-        # default pool behavior
-        for _ in range(255):
-            d_p = d
-            for _x in _xp:
-                d_p = d_p * d // (_x * n_coins)
-            d_prev = d
-            d = (
-                (a_nn * s // self.A_PRECISION + d_p * n_coins)
-                * d
-                // ((a_nn - self.A_PRECISION) * d // self.A_PRECISION + (n_coins + 1) * d_p)
-            )
+        for _ in range(255):  # pragma: no branch
+            d_p = d_prev = d
+            d_p = dp_func(d=d, d_p=d_p, xp=_xp)
+            d = d_func(a_nn=a_nn, s=s, d=d, d_p=d_p, n_coins=n_coins, a_precision=self.A_PRECISION)
             if d_prev < d:
                 if d - d_prev <= 1:
                     return d
             elif d_prev - d <= 1:
                 return d
 
-        raise EVMRevertError(
-            error=f"_get_D() did not converge for pool {self.address}"
-        )  # pragma: no cover
+        raise EVMRevertError(error="D calculation did not converge.")  # pragma: no cover
 
     def _get_y(self, i: int, j: int, x: int, xp: Sequence[int]) -> int:
         """
