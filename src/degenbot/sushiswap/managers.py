cdb5ec3b11405ad72be3383725566856489f430d
BowTiedDevil <devil@bowtieddevil.com>
2024-12-18 12:37:29 -0800

    Specify pool class for Sushiswap managers

--- src/degenbot/sushiswap/managers.py
+++ src/degenbot/sushiswap/managers.py
@@ -1,7 +1,12 @@
+from typing import TypeAlias
+
+from degenbot.sushiswap.pools import SushiswapV2Pool, SushiswapV3Pool
 from degenbot.uniswap.managers import UniswapV2PoolManager, UniswapV3PoolManager
 
 
-class SushiswapV2PoolManager(UniswapV2PoolManager): ...
+class SushiswapV2PoolManager(UniswapV2PoolManager):
+    Pool: TypeAlias = SushiswapV2Pool
 
 
-class SushiswapV3PoolManager(UniswapV3PoolManager): ...
+class SushiswapV3PoolManager(UniswapV3PoolManager):
+    Pool: TypeAlias = SushiswapV3Pool
