3d490c09faa3993025f14a8c9f0fe68d11e55608
BowTiedDevil <devil@bowtieddevil.com>
2025-03-03 10:42:32 -0800

    Lint

--- tests/uniswap/v4/libraries/test_tick_bitmap.py
+++ tests/uniswap/v4/libraries/test_tick_bitmap.py
@@ -5,12 +5,18 @@ from degenbot.constants import MAX_INT24, MIN_INT24
 from degenbot.uniswap.v4_libraries.tick_bitmap import compress, position
 
 # Tests adapted from Foundry tests in the Uniswap V4 Github repo
-# ref:
+# ref: https://github.com/Uniswap/v4-core/blob/main/test/libraries/TickBitmap.t.sol
 
 
 @hypothesis.given(
-    tick=hypothesis.strategies.integers(min_value=MIN_INT24, max_value=MAX_INT24),
-    tick_spacing=hypothesis.strategies.integers(min_value=MIN_INT24, max_value=MAX_INT24),
+    tick=hypothesis.strategies.integers(
+        min_value=MIN_INT24,
+        max_value=MAX_INT24,
+    ),
+    tick_spacing=hypothesis.strategies.integers(
+        min_value=MIN_INT24,
+        max_value=MAX_INT24,
+    ),
 )
 def test_fuzz_compress(tick: int, tick_spacing: int):
     hypothesis.assume(tick_spacing >= 1)
@@ -24,7 +30,10 @@ def test_fuzz_compress(tick: int, tick_spacing: int):
 
 
 @hypothesis.given(
-    tick=hypothesis.strategies.integers(min_value=MIN_INT24, max_value=MAX_INT24),
+    tick=hypothesis.strategies.integers(
+        min_value=MIN_INT24,
+        max_value=MAX_INT24,
+    ),
 )
 def test_fuzz_position(tick: int):
     word_pos, bit_pos = position(tick)
