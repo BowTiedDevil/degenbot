a763b5ef86468ba1d91abe8843045b7abad69c6e
BowTiedDevil <devil@bowtieddevil.com>
2025-03-20 10:33:04 -0700

    refactor:
    use PoolManager address in index
    only process `ModifyLiquidity` event
    use `HexBytes` for pool ID
    increase default block range from 100 to 1000

--- src/degenbot/uniswap/v4_snapshot.py
+++ src/degenbot/uniswap/v4_snapshot.py
@@ -4,7 +4,7 @@ import pathlib
 from typing import Any, cast
 
 import pydantic_core
-from eth_typing import ABIEvent, ChecksumAddress
+from eth_typing import ABIEvent, ChecksumAddress, HexStr
 from eth_utils.abi import event_abi_to_log_topic
 from hexbytes import HexBytes
 from web3 import Web3
@@ -32,6 +32,7 @@ class UniswapV4LiquiditySnapshot:
     def __init__(
         self,
         file: pathlib.Path | str,
+        pool_manager_address: ChecksumAddress | str,
         chain_id: int | None = None,
     ):
         if isinstance(file, str):
@@ -39,10 +40,17 @@ class UniswapV4LiquiditySnapshot:
         json_liquidity_snapshot: dict[str, Any] = pydantic_core.from_json(file.read_bytes())
 
         self._chain_id = chain_id if chain_id is not None else connection_manager.default_chain_id
+        self.pool_manager_address = get_checksum_address(pool_manager_address)
         self.newest_block = json_liquidity_snapshot.pop("snapshot_block")
 
-        self._liquidity_snapshot: dict[ChecksumAddress, dict[str, Any]] = {
-            pool_id: {
+        self._liquidity_snapshot: dict[
+            tuple[
+                ChecksumAddress,  # PoolManager address
+                HexBytes,  # PoolId
+            ],
+            dict[str, Any],
+        ] = {
+            (self.pool_manager_address, HexBytes(pool_id)): {
                 "tick_bitmap": {
                     int(k): UniswapV4BitmapAtWord(**v)
                     for k, v in pool_liquidity_snapshot["tick_bitmap"].items()
@@ -59,50 +67,51 @@ class UniswapV4LiquiditySnapshot:
             f"Loaded LP snapshot: {len(json_liquidity_snapshot)} pools @ block {self.newest_block}"
         )
 
-        self._liquidity_events: dict[ChecksumAddress, list[UniswapV4LiquidityEvent]] = {}
+        self._liquidity_events: dict[
+            tuple[ChecksumAddress, HexBytes],  # PoolManager address, PoolId
+            list[UniswapV4LiquidityEvent],
+        ] = {}
 
     @property
     def chain_id(self) -> int:
         return self._chain_id
 
     def _add_pool_if_missing(
-        self, pool_manager_address: ChecksumAddress, pool_id: HexBytes
+        self,
+        pool_manager_address: ChecksumAddress,
+        pool_id: HexBytes,
     ) -> None:
         """
-        Create empty containers for the pool manager and pool ID, if missing.
+        Create entries for the pool manager and pool ID if missing.
         """
 
-        try:
-            self._liquidity_events[pool_manager_address]
-        except KeyError:
-            self._liquidity_events[pool_manager_address] = {}
+        if (pool_manager_address, pool_id) not in self._liquidity_events:
+            self._liquidity_events[(pool_manager_address, pool_id)] = []
 
-        try:
-            self._liquidity_events[pool_manager_address][pool_id]
-        except KeyError:
-            self._liquidity_events[pool_manager_address][pool_id] = []
-
-        try:
-            self._liquidity_snapshot[pool_manager_address]
-        except KeyError:
-            self._liquidity_snapshot[pool_manager_address] = {}
-
-        try:
-            self._liquidity_snapshot[pool_manager_address][pool_id]
-        except KeyError:
-            self._liquidity_snapshot[pool_manager_address][pool_id] = {}
+        if (pool_manager_address, pool_id) not in self._liquidity_snapshot:
+            self._liquidity_snapshot[(pool_manager_address, pool_id)] = {}
 
     def fetch_new_liquidity_events(
         self,
         to_block: int,
-        span: int = 100,
+        blocks_per_request: int = 1000,
     ) -> None:
+        """
+        Fetch liquidity events from the newest known liquidity events to the target block using
+        `eth_getLogs`. Blocks per request will be capped at `blocks_per_request`.
+        """
+
         def process_liquidity_event_log(
             event: BaseContractEvent, log: LogReceipt
-        ) -> tuple[ChecksumAddress, str, UniswapV4LiquidityEvent]:
+        ) -> tuple[ChecksumAddress, HexBytes, UniswapV4LiquidityEvent]:
+            """
+            Decode an event log and convert to an address, pool ID, and a `UniswapV4LiquidityEvent`
+            for processing with `UniswapV4Pool.update_liquidity_map`.
+            """
+
             decoded_event: EventData = event.process_log(log)
             pool_manager_address = get_checksum_address(decoded_event["address"])
-            pool_id = HexBytes(decoded_event["args"]["id"]).to_0x_hex()
+            pool_id = HexBytes(decoded_event["args"]["id"])
             tx_index = decoded_event["transactionIndex"]
             liquidity_block = decoded_event["blockNumber"]
             liquidity = decoded_event["args"]["liquidityDelta"]
@@ -121,70 +130,96 @@ class UniswapV4LiquiditySnapshot:
                 ),
             )
 
-        logger.info(f"Updating snapshot from block {self.newest_block} to {to_block}")
+        logger.info(f"Updating Uniswap V4 snapshot from block {self.newest_block} to {to_block}")
         w3 = connection_manager.get_web3(self.chain_id)
 
-        # event ModifyLiquidity(id, msg.sender, params.tickLower, params.tickUpper, params.liquidityDelta, params.salt);
+        """
+        Event definition
+        ref: https://github.com/Uniswap/v4-core/blob/main/src/interfaces/IPoolManager.sol
+
+        event ModifyLiquidity(
+          id,
+          msg.sender,
+          params.tickLower,
+          params.tickUpper,
+          params.liquidityDelta,
+          params.salt
+        );
+        """
 
         v4_pool_manager = Web3().eth.contract(abi=UNISWAP_V4_POOL_MANAGER_ABI)
-        for event in [v4_pool_manager.events.ModifyLiquidity]:
-            event_instance = event()
-            logger.info(f"Processing {event.event_name} events")
-            event_abi = cast(
-                "ABIEvent",
-                get_abi_element(abi=v4_pool_manager.abi, abi_element_identifier=event.event_name),
+        event = v4_pool_manager.events.ModifyLiquidity
+        event_instance = event()
+        logger.info(f"Processing {event.event_name} events")
+        event_abi = cast(
+            "ABIEvent",
+            get_abi_element(abi=v4_pool_manager.abi, abi_element_identifier=event.event_name),
+        )
+        start_block = self.newest_block + 1
+
+        while True:
+            end_block = min(to_block, start_block + blocks_per_request - 1)
+
+            event_filter_params = FilterParams(
+                fromBlock=start_block,
+                toBlock=end_block,
+                topics=[HexBytes(event_abi_to_log_topic(event_abi))],
             )
-            start_block = self.newest_block + 1
 
-            while True:
-                end_block = min(to_block, start_block + span - 1)
-
-                event_filter_params = FilterParams(
-                    fromBlock=start_block,
-                    toBlock=end_block,
-                    topics=[HexBytes(event_abi_to_log_topic(event_abi))],
+            for event_log in w3.eth.get_logs(event_filter_params):
+                pool_manager_address, pool_id, liquidity_event = process_liquidity_event_log(
+                    event_instance, event_log
                 )
 
-                for event_log in w3.eth.get_logs(event_filter_params):
-                    pool_manager_address, pool_id, liquidity_event = process_liquidity_event_log(
-                        event_instance, event_log
-                    )
+                if liquidity_event.liquidity == 0:  # pragma: no cover
+                    continue
 
-                    if liquidity_event.liquidity == 0:  # pragma: no cover
-                        continue
+                self._add_pool_if_missing(pool_manager_address, pool_id)
+                self._liquidity_events[(pool_manager_address, pool_id)].append(liquidity_event)
 
-                    self._add_pool_if_missing(pool_manager_address, pool_id)
-                    self._liquidity_events[pool_manager_address][pool_id].append(liquidity_event)
-
-                if end_block == to_block:
-                    break
-                start_block = end_block + 1
+            if end_block == to_block:
+                break
+            start_block = end_block + 1
 
         logger.info(f"Updated snapshot to block {to_block}")
         self.newest_block = to_block
 
     def get_new_liquidity_updates(
+        self,
+        pool_manager_address: HexStr,
+        pool_id: bytes | HexStr,
+    ) -> tuple[UniswapV4PoolLiquidityMappingUpdate, ...]:
         pool_manager_address = get_checksum_address(pool_manager_address)
+        pool_id = HexBytes(pool_id)
+
+        # Fetch any pending updates
+        pending_events = self._liquidity_events.get(
+            (pool_manager_address, pool_id),
+            [],
+        )
+
+        # Clear pending events for this pool
+        self._liquidity_events[(pool_manager_address, pool_id)] = []
+
+        return tuple(
             UniswapV4PoolLiquidityMappingUpdate(
                 block_number=event.block_number,
                 liquidity=event.liquidity,
                 tick_lower=event.tick_lower,
                 tick_upper=event.tick_upper,
             )
-            for event in sorted_events
-        ]
+            for event in pending_events
+        )
 
     def get_tick_bitmap(
         self,
-        pool_manager_address: ChecksumAddress | str,
-        pool_id: str,
+        pool_manager_address: ChecksumAddress | HexStr,
+        pool_id: bytes | HexStr,
     ) -> dict[int, UniswapV4BitmapAtWord]:
-        pool_manager_address = to_checksum_address(pool_manager_address)
-
         try:
             tick_bitmap: dict[int, UniswapV4BitmapAtWord] = self._liquidity_snapshot[
-                pool_manager_address
-            ][pool_id]["tick_bitmap"]
+                (get_checksum_address(pool_manager_address), HexBytes(pool_id))
+            ]["tick_bitmap"]
         except KeyError:
             return {}
         else:
@@ -193,14 +228,12 @@ class UniswapV4LiquiditySnapshot:
     def get_tick_data(
         self,
         pool_manager_address: ChecksumAddress | str,
-        pool_id: str,
+        pool_id: bytes | str,
     ) -> dict[int, UniswapV4LiquidityAtTick]:
-        pool_manager_address = to_checksum_address(pool_manager_address)
-
         try:
             tick_data: dict[int, UniswapV4LiquidityAtTick] = self._liquidity_snapshot[
-                pool_manager_address
-            ][pool_id]["tick_data"]
+                (get_checksum_address(pool_manager_address), HexBytes(pool_id))
+            ]["tick_data"]
         except KeyError:
             return {}
         else:
@@ -208,20 +241,18 @@ class UniswapV4LiquiditySnapshot:
 
     def update_snapshot(
         self,
-        pool: ChecksumAddress | str,
+        pool_manager_address: ChecksumAddress | HexStr,
+        pool_id: bytes | HexStr,
         tick_data: dict[int, UniswapV4LiquidityAtTick],
         tick_bitmap: dict[int, UniswapV4BitmapAtWord],
     ) -> None:
-        pool_address = to_checksum_address(pool)
+        pool_manager_address = get_checksum_address(pool_manager_address)
+        pool_id = HexBytes(pool_id)
 
-        self._add_pool_if_missing(pool_address)
-        self._liquidity_snapshot[pool_address].update(
+        self._add_pool_if_missing(pool_manager_address=pool_manager_address, pool_id=pool_id)
+        self._liquidity_snapshot[(pool_manager_address, pool_id)].update(
             {
                 "tick_bitmap": tick_bitmap,
-            }
-        )
-        self._liquidity_snapshot[pool_address].update(
-            {
                 "tick_data": tick_data,
             }
         )
