e49bc02fa124eeac3d08f0a570120f3daa09653e
BowTiedDevil <devil@bowtieddevil.com>
2025-03-24 15:54:15 -0700

    chore: increase cache depth

--- src/degenbot/cache.py
+++ src/degenbot/cache.py
@@ -4,6 +4,6 @@ from eth_typing import ChecksumAddress, HexStr
 from eth_utils.address import to_checksum_address
 
 
-@functools.lru_cache
+@functools.lru_cache(maxsize=512)
 def get_checksum_address(address: HexStr | bytes) -> ChecksumAddress:
     return to_checksum_address(address)
