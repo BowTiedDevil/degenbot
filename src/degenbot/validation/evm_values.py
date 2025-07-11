1bb0b7a86a248d7789d344a69f8d5dd260a545a2
BowTiedDevil <devil@bowtieddevil.com>
2025-07-11 16:57:28 -0700

    refactor: drop `strict` argument for validated types

--- src/degenbot/validation/evm_values.py
+++ src/degenbot/validation/evm_values.py
@@ -23,16 +23,16 @@ from degenbot.constants import (
     MIN_UINT256,
 )
 
-type ValidatedInt16 = Annotated[int, Field(strict=True, ge=MIN_INT16, le=MAX_INT16)]
-type ValidatedInt24 = Annotated[int, Field(strict=True, ge=MIN_INT24, le=MAX_INT24)]
-type ValidatedInt128 = Annotated[int, Field(strict=True, ge=MIN_INT128, le=MAX_INT128)]
-type ValidatedInt256 = Annotated[int, Field(strict=True, ge=MIN_INT256, le=MAX_INT256)]
+type ValidatedInt16 = Annotated[int, Field(ge=MIN_INT16, le=MAX_INT16)]
+type ValidatedInt24 = Annotated[int, Field(ge=MIN_INT24, le=MAX_INT24)]
+type ValidatedInt128 = Annotated[int, Field(ge=MIN_INT128, le=MAX_INT128)]
+type ValidatedInt256 = Annotated[int, Field(ge=MIN_INT256, le=MAX_INT256)]
 
-type ValidatedUint8 = Annotated[int, Field(strict=True, ge=MIN_UINT8, le=MAX_UINT8)]
-type ValidatedUint24 = Annotated[int, Field(strict=True, ge=MIN_UINT24, le=MAX_UINT24)]
-type ValidatedUint128 = Annotated[int, Field(strict=True, ge=MIN_UINT128, le=MAX_UINT128)]
-type ValidatedUint128NonZero = Annotated[int, Field(strict=True, gt=MIN_UINT128, le=MAX_UINT128)]
-type ValidatedUint160 = Annotated[int, Field(strict=True, ge=MIN_UINT160, le=MAX_UINT160)]
-type ValidatedUint160NonZero = Annotated[int, Field(strict=True, gt=MIN_UINT160, le=MAX_UINT160)]
-type ValidatedUint256 = Annotated[int, Field(strict=True, ge=MIN_UINT256, le=MAX_UINT256)]
-type ValidatedUint256NonZero = Annotated[int, Field(strict=True, gt=MIN_UINT256, le=MAX_UINT256)]
+type ValidatedUint8 = Annotated[int, Field(ge=MIN_UINT8, le=MAX_UINT8)]
+type ValidatedUint24 = Annotated[int, Field(ge=MIN_UINT24, le=MAX_UINT24)]
+type ValidatedUint128 = Annotated[int, Field(ge=MIN_UINT128, le=MAX_UINT128)]
+type ValidatedUint128NonZero = Annotated[int, Field(gt=MIN_UINT128, le=MAX_UINT128)]
+type ValidatedUint160 = Annotated[int, Field(ge=MIN_UINT160, le=MAX_UINT160)]
+type ValidatedUint160NonZero = Annotated[int, Field(gt=MIN_UINT160, le=MAX_UINT160)]
+type ValidatedUint256 = Annotated[int, Field(ge=MIN_UINT256, le=MAX_UINT256)]
+type ValidatedUint256NonZero = Annotated[int, Field(gt=MIN_UINT256, le=MAX_UINT256)]
