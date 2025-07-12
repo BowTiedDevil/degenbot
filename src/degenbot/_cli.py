70c3ded335c977c522f8d51d7319761e84fe0286
BowTiedDevil <devil@bowtieddevil.com>
2025-07-11 17:01:54 -0700

    fix: import

--- src/degenbot/_cli.py
+++ src/degenbot/_cli.py
@@ -6,7 +6,7 @@ import tomlkit
 from pydantic import TypeAdapter
 
 from degenbot import __version__, settings
-from degenbot.database import create_new_sqlite_database, update_existing_sqlite_database
+from degenbot.database import create_new_sqlite_database, upgrade_existing_sqlite_database
 
 
 @click.group()
