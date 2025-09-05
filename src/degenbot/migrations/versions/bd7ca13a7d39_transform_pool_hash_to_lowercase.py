ac036cbb6d8dae0ef7f5e28554dd46e22f282acb
BowTiedDevil <devil@bowtieddevil.com>
2025-09-05 14:29:43 -0700

    db: transform pool hash to lowercase
    HEX() transformation used earlier results in uppercase string

---
+++ src/degenbot/migrations/versions/bd7ca13a7d39_transform_pool_hash_to_lowercase.py
@@ -0,0 +1,32 @@
+"""Transform pool hash to lowercase
+
+Revision ID: bd7ca13a7d39
+Revises: d1f98e2c3b18
+Create Date: 2025-09-05 14:19:57.457685
+
+"""
+
+from collections.abc import Sequence
+
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision: str = "bd7ca13a7d39"
+down_revision: str | Sequence[str] | None = "d1f98e2c3b18"
+branch_labels: str | Sequence[str] | None = None
+depends_on: str | Sequence[str] | None = None
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    op.execute(
+        """
+        UPDATE uniswap_v4_pools
+        SET pool_hash = LOWER(pool_hash)
+        """
+    )
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    raise NotImplementedError("Downgrade is not supported for this migration.")
