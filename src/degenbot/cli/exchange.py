5e72a57da381af9ad2f97b145c6996cce98c2d2d
BowTiedDevil <devil@bowtieddevil.com>
2025-11-10 10:52:34 -0800

    feat: take addresses from deployments

--- src/degenbot/cli/exchange.py
+++ src/degenbot/cli/exchange.py
@@ -2,12 +2,30 @@ import click
 import eth_typing
 from sqlalchemy import select
 
-from degenbot.checksum_cache import get_checksum_address
 from degenbot.cli import cli
 from degenbot.config import CONFIG_FILE, settings
 from degenbot.database import db_session
 from degenbot.database.models.base import ExchangeTable
 from degenbot.database.models.pools import PoolManagerTable
+from degenbot.uniswap.deployments import (
+    BaseAerodromeV2,
+    BaseAerodromeV3,
+    BasePancakeswapV2,
+    BasePancakeswapV3,
+    BaseSushiswapV2,
+    BaseSushiswapV3,
+    BaseSwapbasedV2,
+    BaseUniswapV2,
+    BaseUniswapV3,
+    BaseUniswapV4,
+    EthereumMainnetPancakeswapV2,
+    EthereumMainnetPancakeswapV3,
+    EthereumMainnetSushiswapV2,
+    EthereumMainnetSushiswapV3,
+    EthereumMainnetUniswapV2,
+    EthereumMainnetUniswapV3,
+    EthereumMainnetUniswapV4,
+)
 
 
 @cli.group()
@@ -70,7 +88,7 @@ def activate_base_aerodrome_v2(
                 chain_id=chain_id,
                 name=exchange_name,
                 active=True,
-                factory=get_checksum_address("0x420DD381b31aEf6683db6B902084cB0FFECe40Da"),
+                factory=BaseAerodromeV2.factory.address,
             )
         )
         db_session.commit()
@@ -108,7 +126,7 @@ def activate_base_aerodrome_v3(
                 chain_id=chain_id,
                 name=exchange_name,
                 active=True,
-                factory=get_checksum_address("0x5e7BB104d84c7CB9B682AaC2F3d509f5F406809A"),
+                factory=BaseAerodromeV3.factory.address,
             )
         )
         db_session.commit()
@@ -146,7 +164,7 @@ def activate_base_pancakeswap_v2(
                 chain_id=chain_id,
                 name=exchange_name,
                 active=True,
-                factory=get_checksum_address("0x02a84c1b3BBD7401a5f7fa98a384EBC70bB5749E"),
+                factory=BasePancakeswapV2.factory.address,
             )
         )
         db_session.commit()
@@ -184,8 +202,8 @@ def activate_base_pancakeswap_v3(
                 chain_id=chain_id,
                 name=exchange_name,
                 active=True,
-                factory=get_checksum_address("0x0BFbCF9fa4f9C56B0F40a671Ad40E0805A091865"),
-                deployer=get_checksum_address("0x41ff9AA7e16B8B1a8a8dc4f0eFacd93D02d071c9"),
+                factory=BasePancakeswapV3.factory.address,
+                deployer=BasePancakeswapV3.factory.deployer,
             )
         )
         db_session.commit()
@@ -223,7 +241,7 @@ def activate_base_swapbased_v2(
                 chain_id=chain_id,
                 name=exchange_name,
                 active=True,
-                factory=get_checksum_address("0x04C9f118d21e8B767D2e50C946f0cC9F6C367300"),
+                factory=BaseSwapbasedV2.factory.address,
             )
         )
         db_session.commit()
@@ -261,7 +279,7 @@ def activate_base_sushiswap_v2(
                 chain_id=chain_id,
                 name=exchange_name,
                 active=True,
-                factory=get_checksum_address("0x71524B4f93c58fcbF659783284E38825f0622859"),
+                factory=BaseSushiswapV2.factory.address,
             )
         )
         db_session.commit()
@@ -299,7 +317,7 @@ def activate_base_sushiswap_v3(
                 chain_id=chain_id,
                 name=exchange_name,
                 active=True,
-                factory=get_checksum_address("0xc35DADB65012eC5796536bD9864eD8773aBc74C4"),
+                factory=BaseSushiswapV3.factory.address,
             )
         )
         db_session.commit()
@@ -337,7 +355,7 @@ def activate_base_uniswap_v2(
                 chain_id=chain_id,
                 name=exchange_name,
                 active=True,
-                factory=get_checksum_address("0x8909Dc15e40173Ff4699343b6eB8132c65e18eC6"),
+                factory=BaseUniswapV2.factory.address,
             )
         )
         db_session.commit()
@@ -375,7 +393,7 @@ def activate_base_uniswap_v3(
                 chain_id=chain_id,
                 name=exchange_name,
                 active=True,
-                factory=get_checksum_address("0x33128a8fC17869897dcE68Ed026d694621f6FDfD"),
+                factory=BaseUniswapV3.factory.address,
             )
         )
         db_session.commit()
@@ -394,7 +412,6 @@ def activate_base_uniswap_v4(
 
     _check_configured_rpc(chain_id)
 
-    pool_manager_address = get_checksum_address("0x498581fF718922c3f8e6A244956aF099B2652b2b")
     exchange_kind = "uniswap_v4"
 
     exchange = db_session.scalar(
@@ -415,25 +432,25 @@ def activate_base_uniswap_v4(
             chain_id=chain_id,
             name=exchange_name,
             active=True,
-            factory=pool_manager_address,
+            factory=BaseUniswapV4.pool_manager.address,
         )
         db_session.add(exchange)
         db_session.flush()
 
         manager_in_db = db_session.scalar(
             select(PoolManagerTable).where(
-                PoolManagerTable.address == pool_manager_address,
+                PoolManagerTable.address == BaseUniswapV4.pool_manager.address,
                 PoolManagerTable.chain == chain_id,
             )
         )
         if manager_in_db is None:
             db_session.add(
                 PoolManagerTable(
-                    address=pool_manager_address,
+                    address=BaseUniswapV4.pool_manager.address,
                     chain=chain_id,
                     kind=exchange_kind,
                     exchange_id=exchange.id,
-                    state_view=get_checksum_address("0xA3c0c9b65baD0b08107Aa264b0f3dB444b867A71"),
+                    state_view=BaseUniswapV4.state_view.address,
                 )
             )
 
@@ -472,7 +489,7 @@ def activate_ethereum_pancakeswap_v2(
                 chain_id=chain_id,
                 name=exchange_name,
                 active=True,
-                factory=get_checksum_address("0x1097053Fd2ea711dad45caCcc45EfF7548fCB362"),
+                factory=EthereumMainnetPancakeswapV2.factory.address,
             )
         )
         db_session.commit()
@@ -510,8 +527,8 @@ def activate_ethereum_pancakeswap_v3(
                 chain_id=chain_id,
                 name=exchange_name,
                 active=True,
-                factory=get_checksum_address("0x0BFbCF9fa4f9C56B0F40a671Ad40E0805A091865"),
-                deployer=get_checksum_address("0x41ff9AA7e16B8B1a8a8dc4f0eFacd93D02d071c9"),
+                factory=EthereumMainnetPancakeswapV3.factory.address,
+                deployer=EthereumMainnetPancakeswapV3.factory.deployer,
             )
         )
         db_session.commit()
@@ -549,7 +566,7 @@ def activate_ethereum_sushiswap_v2(
                 chain_id=chain_id,
                 name=exchange_name,
                 active=True,
-                factory=get_checksum_address("0xC0AEe478e3658e2610c5F7A4A2E1777cE9e4f2Ac"),
+                factory=EthereumMainnetSushiswapV2.factory.address,
             )
         )
         db_session.commit()
@@ -587,7 +604,7 @@ def activate_ethereum_sushiswap_v3(
                 chain_id=chain_id,
                 name=exchange_name,
                 active=True,
-                factory=get_checksum_address("0xbACEB8eC6b9355Dfc0269C18bac9d6E2Bdc29C4F"),
+                factory=EthereumMainnetSushiswapV3.factory.address,
             )
         )
         db_session.commit()
@@ -625,7 +642,7 @@ def activate_ethereum_uniswap_v2(
                 chain_id=chain_id,
                 name=exchange_name,
                 active=True,
-                factory=get_checksum_address("0x5c69bee701ef814a2b6a3edd4b1652cb9cc5aa6f"),
+                factory=EthereumMainnetUniswapV2.factory.address,
             )
         )
         db_session.commit()
@@ -663,7 +680,7 @@ def activate_ethereum_uniswap_v3(
                 chain_id=chain_id,
                 name=exchange_name,
                 active=True,
-                factory=get_checksum_address("0x1f98431c8ad98523631ae4a59f267346ea31f984"),
+                factory=EthereumMainnetUniswapV3.factory.address,
             )
         )
         db_session.commit()
@@ -682,7 +699,6 @@ def activate_ethereum_uniswap_v4(
 
     _check_configured_rpc(chain_id)
 
-    pool_manager_address = get_checksum_address("0x000000000004444c5dc75cB358380D2e3dE08A90")
     exchange_kind = "uniswap_v4"
 
     exchange = db_session.scalar(
@@ -703,25 +719,25 @@ def activate_ethereum_uniswap_v4(
             chain_id=chain_id,
             name=exchange_name,
             active=True,
-            factory=pool_manager_address,
+            factory=EthereumMainnetUniswapV4.pool_manager.address,
         )
         db_session.add(exchange)
         db_session.flush()
 
         manager_in_db = db_session.scalar(
             select(PoolManagerTable).where(
-                PoolManagerTable.address == pool_manager_address,
+                PoolManagerTable.address == EthereumMainnetUniswapV4.pool_manager.address,
                 PoolManagerTable.chain == chain_id,
             )
         )
         if manager_in_db is None:
             db_session.add(
                 PoolManagerTable(
-                    address=pool_manager_address,
+                    address=EthereumMainnetUniswapV4.pool_manager.address,
                     chain=chain_id,
                     kind=exchange_kind,
                     exchange_id=exchange.id,
-                    state_view=get_checksum_address("0x7fFE42C4a5DEeA5b0feC41C94C136Cf115597227"),
+                    state_view=EthereumMainnetUniswapV4.state_view.address,
                 )
             )
 
