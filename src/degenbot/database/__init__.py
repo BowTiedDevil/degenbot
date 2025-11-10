3c65375cd03cafaa4a5e444446506e52262178e9
BowTiedDevil <devil@bowtieddevil.com>
2025-11-10 13:40:25 -0800

    feat: set DB snapshot immediately

--- src/degenbot/database/__init__.py
+++ src/degenbot/database/__init__.py
@@ -1,15 +1,20 @@
 from alembic.runtime.migration import MigrationContext
 from alembic.script import ScriptDirectory
+from sqlalchemy import text
 
 from degenbot.config import settings
 from degenbot.database.operations import get_alembic_config, get_scoped_sqlite_session
 from degenbot.logging import logger
 from degenbot.version import __version__
 
-db_session = get_scoped_sqlite_session(database_path=settings.database.path)
+db_session = get_scoped_sqlite_session(database_path=settings.database.path)()
+
+# BEGIN a transaction to establish a checkpoint. This prevents the session from reading inconsistent
+# values due to concurrent writes by a separate session initiated after this one.
+db_session.connection().execute(text("BEGIN"))
 
 current_database_version = MigrationContext.configure(
-    connection=db_session().connection()
+    connection=db_session.connection()
 ).get_current_revision()
 latest_database_version = ScriptDirectory.from_config(
     config=get_alembic_config()
