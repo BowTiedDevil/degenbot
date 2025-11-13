72e5da8829f8b2e14fee33a0d24a84b1b4c3adca
BowTiedDevil <devil@bowtieddevil.com>
2025-11-13 10:57:48 -0800

    feat(pathfinding):
    add token equivalence for 2-pool case
    add pool type selection

--- src/degenbot/pathfinding.py
+++ src/degenbot/pathfinding.py
@@ -7,29 +7,38 @@ from eth_typing import ChecksumAddress
 
 from degenbot.database import db_session
 from degenbot.database.models.erc20 import Erc20TokenTable
-from degenbot.database.models.pools import LiquidityPoolTable, ManagedLiquidityPoolTable
+from degenbot.database.models.pools import LiquidityPoolTable, UniswapV4PoolTable
 from degenbot.exceptions.base import DegenbotValueError
+from degenbot.logging import logger
 
 
 def find_paths(
     chain_id: int,
     start_token: ChecksumAddress,
     end_token: ChecksumAddress,
+    min_depth: int = 2,
     max_depth: int | None = None,
-    pool_types: Sequence[LiquidityPoolTable | ManagedLiquidityPoolTable] = (
-        LiquidityPoolTable,
-        ManagedLiquidityPoolTable,
-    ),
-) -> Iterator[Sequence[ChecksumAddress]]:
+    pool_types: Sequence[type] = [LiquidityPoolTable, UniswapV4PoolTable],
+    equivalent_tokens: Sequence[tuple[ChecksumAddress, ChecksumAddress]] | None = None,
+) -> Iterator[Sequence[LiquidityPoolTable | UniswapV4PoolTable]]:
     """
     Find paths from `start_token` to `end_token`, formatted as an iterator of pool objects
     constructed from the database.
 
-    Paths are discovered using the breadth-first search strategy with an optional maximum depth.
+    Paths are yielded using a breadth-first search strategy starting from a minimum depth up to an
+    optional maximum.
 
-    Paths may be limited to a subset of pool types. If not specified, all valid pool types will be
-    included.
+    Paths may be constrained to a subset of pool types. If not specified, all valid pool types will
+    be included.
+
+    Pairs of equivalent profit tokens may be provided as a tuple of addresses. These tokens are
+    assumed be convertible by some other means outside of the swap path, e.g. native Ether and WETH
+    can be converted at 1:1 through the WETH contract.
     """
+
+    # @dev chain_id is only used to look up the start and end tokens. Liquidity pool lookups use
+    # these IDs, thus they are implicitly filtered for the target chain ID without needing a
+    # dedicated WHERE clause.
 
     start_token_in_db = db_session.scalar(
         sqlalchemy.select(Erc20TokenTable).where(
@@ -51,102 +60,290 @@ def find_paths(
         msg = "The end token was not found in the database."
         raise DegenbotValueError(msg)
 
-    num_standalone_pools = db_session.scalar(
-        sqlalchemy.select(sqlalchemy.func.count())
-        .select_from(
-            *(pool_type for pool_type in pool_types if issubclass(pool_type, LiquidityPoolTable))
-        )
-        .where(LiquidityPoolTable.chain == chain_id)
-    )
-    num_managed_pools = db_session.scalar(
-        sqlalchemy.select(sqlalchemy.func.count())
-        .select_from(
-            *(
-                pool_type
-                for pool_type in pool_types
-                if issubclass(pool_type, ManagedLiquidityPoolTable)
-            )
-        )
-        .where(ManagedLiquidityPoolTable.manager.has(chain=chain_id))
-    )
+    equivalent: dict[ChecksumAddress, ChecksumAddress] = {}
+    if equivalent_tokens is not None:
+        for token0, token1 in equivalent_tokens:
+            equivalent[token0] = token1
+            equivalent[token1] = token0
 
-    num_pools = num_standalone_pools + num_managed_pools
+    pools: list[LiquidityPoolTable | UniswapV4PoolTable]
+    forward_token_ids: set[int]
+    token_id_selects: list[sqlalchemy.Select[tuple[int]]]
 
-    print(f"Finding paths from {num_standalone_pools} standalone pools in database")
-    print(f"Finding paths from {num_managed_pools} managed pools in database")
-    print(f"Finding paths from {num_pools} known pools in database")
+    match max_depth, start_token == end_token:
+        case 2, True:
+            """
+            Cycle a profit token P through two pools with a common forward token X:
+                Pool A holding a P-X pair
+                Pool B holding a P-X pair
 
-    match max_depth:
-        case 2:
-            assert start_token == end_token
+            e.g. cycle WETH through two WETH-X pairs.
+            """
+
+            if equivalent_tokens is not None:
+                logger.warning(
+                    "One or more equivalent token pairings were provided. Equivalent tokens are "
+                    "not relevant for a two-pool cycle with a single profit token, and will be "
+                    "ignored. To find paths with equivalent profit tokens, call this function "
+                    "again with differing start and end tokens."
+                )
+
             cycle_token = start_token_in_db
 
-            candidate_token_ids = db_session.scalars(
-                sqlalchemy.select(
-                    sqlalchemy.union_all(
+            # Assemble the token ID selections for the in-scope pools
+            token_id_selects = []
+            for pool_type in pool_types:
+                if issubclass(pool_type, LiquidityPoolTable):
+                    token_id_selects.append(
                         sqlalchemy.select(
-                            LiquidityPoolTable.token1_id.label("forward_token_id")
-                        ).where(LiquidityPoolTable.token0_id == cycle_token.id),
-                        sqlalchemy.select(
-                            LiquidityPoolTable.token0_id.label("forward_token_id")
-                        ).where(LiquidityPoolTable.token1_id == cycle_token.id),
+                            pool_type.token1_id.label("forward_token_id"),
+                        ).where(
+                            pool_type.token0_id == cycle_token.id,
+                        )
                     )
-                    .subquery()
-                    .columns["forward_token_id"]
-                )
-            ).all()
-
-            print(f"Found {len(candidate_token_ids)} cycle token neighbors")
+                    token_id_selects.append(
+                        sqlalchemy.select(
+                            pool_type.token0_id.label("forward_token_id"),
+                        ).where(
+                            pool_type.token1_id == cycle_token.id,
+                        )
+                    )
+                if issubclass(pool_type, UniswapV4PoolTable):
+                    token_id_selects.append(
+                        sqlalchemy.select(
+                            pool_type.currency1_id.label("forward_token_id"),
+                        ).where(
+                            pool_type.currency0_id == cycle_token.id,
+                        )
+                    )
+                    token_id_selects.append(
+                        sqlalchemy.select(
+                            pool_type.currency0_id.label("forward_token_id"),
+                        ).where(
+                            pool_type.currency1_id == cycle_token.id,
+                        )
+                    )
 
             # Identify tokens paired with the cycle token in at least two pools
-            paired_token_ids = db_session.scalars(
-                sqlalchemy.select(
-                    sqlalchemy.union_all(
-                        sqlalchemy.select(
-                            LiquidityPoolTable.token1_id.label("forward_token_id")
-                        ).where(LiquidityPoolTable.token0_id == cycle_token.id),
-                        sqlalchemy.select(
-                            LiquidityPoolTable.token0_id.label("forward_token_id")
-                        ).where(LiquidityPoolTable.token1_id == cycle_token.id),
+            forward_token_ids = set(
+                db_session.scalars(
+                    sqlalchemy.select(
+                        sqlalchemy.union_all(*token_id_selects)
+                        .subquery()
+                        .columns["forward_token_id"]
                     )
-                    .subquery()
-                    .columns["forward_token_id"]
+                    .group_by("forward_token_id")
+                    .having(sqlalchemy.func.count() > 1)
+                ).all()
+            )
+            forward_token_ids.discard(cycle_token.id)
+            logger.debug(f"Found {len(forward_token_ids)} forward tokens")
+
+            for forward_token_id in forward_token_ids:
+                forward_token = db_session.scalar(
+                    sqlalchemy.select(Erc20TokenTable).where(Erc20TokenTable.id == forward_token_id)
                 )
-                .group_by("forward_token_id")
-                .having(sqlalchemy.func.count() > 1)
-            ).all()
-            assert len(set(paired_token_ids)) == len(paired_token_ids)
+                if TYPE_CHECKING:
+                    assert forward_token is not None
 
-            print(f"Found {len(paired_token_ids)} candidate tokens")
+                if forward_token.address.lower() < cycle_token.address.lower():
+                    token0_id, token1_id = forward_token.id, cycle_token.id
+                else:
+                    token0_id, token1_id = cycle_token.id, forward_token.id
 
-            for paired_token_id in paired_token_ids:
-                paired_token_address = db_session.scalar(
-                    sqlalchemy.select(Erc20TokenTable.address).where(
-                        Erc20TokenTable.id == paired_token_id,
+                pools = []
+
+                for pool_type in pool_types:
+                    if issubclass(pool_type, LiquidityPoolTable):
+                        pools.extend(
+                            db_session.scalars(
+                                sqlalchemy.select(pool_type).where(
+                                    pool_type.token0_id == token0_id,
+                                    pool_type.token1_id == token1_id,
+                                )
+                            ).all()
+                        )
+                    if issubclass(pool_type, UniswapV4PoolTable):
+                        pools.extend(
+                            db_session.scalars(
+                                sqlalchemy.select(pool_type).where(
+                                    pool_type.currency0_id == token0_id,
+                                    pool_type.currency1_id == token1_id,
+                                )
+                            ).all()
+                        )
+
+                yield from itertools.permutations(pools, 2)
+
+        case 2, False:
+            """
+            Cycle two equivalent profit tokens (P, P') through two pools with a common forward token
+            X:
+                Pool A holding a P-X pair
+                Pool B holding a P'-X pair
+
+            e.g. cycle WETH through a WETH-X pair and an Ether-X pair, or cycle Ether through an
+            Ether-X and WETH-X pair.
+            """
+
+            # For a 2-pool arb with differing cycle start & end tokens, they must be equivalent
+            # e.g. WETH <--> Ether
+            assert equivalent[start_token] == end_token
+            assert equivalent[end_token] == start_token
+
+            cycle_token_start = start_token_in_db
+            cycle_token_end = end_token_in_db
+
+            # Assemble the token ID selections for the in-scope pools
+            token_id_selects = []
+            for pool_type in pool_types:
+                if issubclass(pool_type, LiquidityPoolTable):
+                    token_id_selects.append(
+                        sqlalchemy.select(
+                            pool_type.token0_id.label("forward_token_id"),
+                        ).where(
+                            pool_type.token1_id == cycle_token_start.id,
+                        )
+                    )
+                    token_id_selects.append(
+                        sqlalchemy.select(
+                            pool_type.token1_id.label("forward_token_id"),
+                        ).where(
+                            pool_type.token0_id == cycle_token_start.id,
+                        )
+                    )
+                    token_id_selects.append(
+                        sqlalchemy.select(
+                            pool_type.token0_id.label("forward_token_id"),
+                        ).where(
+                            pool_type.token1_id == cycle_token_end.id,
+                        )
+                    )
+                    token_id_selects.append(
+                        sqlalchemy.select(
+                            pool_type.token1_id.label("forward_token_id"),
+                        ).where(
+                            pool_type.token0_id == cycle_token_end.id,
+                        )
+                    )
+                if issubclass(pool_type, UniswapV4PoolTable):
+                    token_id_selects.append(
+                        sqlalchemy.select(
+                            pool_type.currency1_id.label("forward_token_id"),
+                        ).where(
+                            pool_type.currency0_id == cycle_token_start.id,
+                        )
+                    )
+                    token_id_selects.append(
+                        sqlalchemy.select(
+                            pool_type.currency0_id.label("forward_token_id"),
+                        ).where(
+                            pool_type.currency1_id == cycle_token_start.id,
+                        )
+                    )
+                    token_id_selects.append(
+                        sqlalchemy.select(
+                            pool_type.currency1_id.label("forward_token_id"),
+                        ).where(
+                            pool_type.currency0_id == cycle_token_end.id,
+                        )
+                    )
+                    token_id_selects.append(
+                        sqlalchemy.select(
+                            pool_type.currency0_id.label("forward_token_id"),
+                        ).where(
+                            pool_type.currency1_id == cycle_token_end.id,
+                        )
+                    )
+
+            # Identify tokens paired with the cycle token (or equivalent) in at least two pools
+            forward_token_ids = set(
+                db_session.scalars(
+                    sqlalchemy.select(
+                        sqlalchemy.union_all(*token_id_selects)
+                        .subquery()
+                        .columns["forward_token_id"]
+                    )
+                    .group_by("forward_token_id")
+                    .having(sqlalchemy.func.count() > 1)
+                ).all()
+            )
+            # Exclude both of the cycle tokens from use as a forward token
+            # e.g. a WETH-Ether pool charging a fee is strictly worse than converting via the
+            # wrapper contract, assuming 1:1 reserves
+            forward_token_ids.discard(cycle_token_start.id)
+            forward_token_ids.discard(cycle_token_end.id)
+            logger.debug(f"Found {len(forward_token_ids)} forward tokens")
+
+            for forward_token_id in forward_token_ids:
+                forward_token = db_session.scalar(
+                    sqlalchemy.select(Erc20TokenTable).where(
+                        Erc20TokenTable.id == forward_token_id,
                     )
                 )
                 if TYPE_CHECKING:
-                    assert paired_token_address is not None
+                    assert forward_token is not None
 
+                pools = []
+
+                # Get P-X pools
                 token0_id, token1_id = (
-                    (paired_token_id, cycle_token.id)
-                    if paired_token_address.lower() < cycle_token.address.lower()
-                    else (cycle_token.id, paired_token_id)
+                    (forward_token_id, cycle_token_start.id)
+                    if forward_token.address.lower() < cycle_token_start.address.lower()
+                    else (cycle_token_start.id, forward_token_id)
                 )
+                for pool_type in pool_types:
+                    if issubclass(pool_type, LiquidityPoolTable):
+                        pools.extend(
+                            db_session.scalars(
+                                sqlalchemy.select(pool_type).where(
+                                    pool_type.token0_id == token0_id,
+                                    pool_type.token1_id == token1_id,
+                                )
+                            ).all()
+                        )
+                    if issubclass(pool_type, UniswapV4PoolTable):
+                        pools.extend(
+                            db_session.scalars(
+                                sqlalchemy.select(pool_type).where(
+                                    pool_type.currency0_id == token0_id,
+                                    pool_type.currency1_id == token1_id,
+                                )
+                            ).all()
+                        )
 
-                candidate_pools = db_session.scalars(
-                    sqlalchemy.select(LiquidityPoolTable).where(
-                        LiquidityPoolTable.token0_id == token0_id,
-                        LiquidityPoolTable.token1_id == token1_id,
-                    ),
-                ).all()
+                # Get P'-X pools
+                token0_id, token1_id = (
+                    (forward_token_id, cycle_token_end.id)
+                    if forward_token.address.lower() < cycle_token_end.address.lower()
+                    else (cycle_token_end.id, forward_token_id)
+                )
+                for pool_type in pool_types:
+                    if issubclass(pool_type, LiquidityPoolTable):
+                        pools.extend(
+                            db_session.scalars(
+                                sqlalchemy.select(pool_type).where(
+                                    pool_type.token0_id == token0_id,
+                                    pool_type.token1_id == token1_id,
+                                )
+                            ).all()
+                        )
+                    if issubclass(pool_type, UniswapV4PoolTable):
+                        pools.extend(
+                            db_session.scalars(
+                                sqlalchemy.select(pool_type).where(
+                                    pool_type.currency0_id == token0_id,
+                                    pool_type.currency1_id == token1_id,
+                                )
+                            ).all()
+                        )
 
-                yield from itertools.permutations(candidate_pools, 2)
+                yield from itertools.permutations(pools, 2)
 
         case 3:
-            ...
+            raise NotImplementedError
         case None:
-            ...
+            raise NotImplementedError
         case _:
             msg = "Invalid max_depth!"
             raise DegenbotValueError(msg)
--- tests/test_pathfinding.py
+++ tests/test_pathfinding.py
@@ -1,12 +1,12 @@
-from cchecksum import to_checksum_address
-
+from degenbot.constants import WRAPPED_NATIVE_TOKENS, ZERO_ADDRESS
+from degenbot.database.models.pools import UniswapV2PoolTable, UniswapV3PoolTable
 from degenbot.pathfinding import find_paths
 
 BASE_CHAIN_ID = 8453
-WETH_BASE_ADDRESS = to_checksum_address("0x4200000000000000000000000000000000000006")
+WETH_BASE_ADDRESS = WRAPPED_NATIVE_TOKENS[BASE_CHAIN_ID]
 
 
-def test_two_pool_pathfinding():
+def test_two_pool_pathfinding_cycling_weth():
     paths = tuple(
         find_paths(
             chain_id=BASE_CHAIN_ID,
@@ -16,4 +16,67 @@ def test_two_pool_pathfinding():
         )
     )
     assert paths
-    print(f"Found {len(paths)} paths")
+    print(f"Found {len(paths)} paths (WETH)")
+
+
+def test_two_pool_pathfinding_cycling_weth_with_limited_types():
+    paths = tuple(
+        find_paths(
+            chain_id=BASE_CHAIN_ID,
+            start_token=WETH_BASE_ADDRESS,
+            end_token=WETH_BASE_ADDRESS,
+            pool_types=[UniswapV2PoolTable, UniswapV3PoolTable],
+            max_depth=2,
+        )
+    )
+    assert paths
+    print(f"Found {len(paths)} paths (WETH, Uniswap V2 pools only)")
+
+
+def test_two_pool_pathfinding_cycling_native():
+    paths = tuple(
+        find_paths(
+            chain_id=BASE_CHAIN_ID,
+            start_token=ZERO_ADDRESS,
+            end_token=ZERO_ADDRESS,
+            max_depth=2,
+        )
+    )
+    assert paths
+    print(f"Found {len(paths)} paths (Native Ether)")
+
+
+def test_two_pool_pathfinding_cycling_weth_native_equivalent():
+    paths = tuple(
+        find_paths(
+            chain_id=BASE_CHAIN_ID,
+            start_token=WETH_BASE_ADDRESS,
+            end_token=ZERO_ADDRESS,
+            max_depth=2,
+            equivalent_tokens=[
+                (
+                    WETH_BASE_ADDRESS,
+                    ZERO_ADDRESS,
+                )
+            ],
+        )
+    )
+    assert paths
+    print(f"Found {len(paths)} paths (WETH -> Ether) with equivalents")
+
+    paths = tuple(
+        find_paths(
+            chain_id=BASE_CHAIN_ID,
+            start_token=ZERO_ADDRESS,
+            end_token=WETH_BASE_ADDRESS,
+            max_depth=2,
+            equivalent_tokens=[
+                (
+                    WETH_BASE_ADDRESS,
+                    ZERO_ADDRESS,
+                )
+            ],
+        )
+    )
+    assert paths
+    print(f"Found {len(paths)} paths (Ether -> WETH) with equivalents")
