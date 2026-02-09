b3ca205c691160473f8e415ad5b62ee343dca503
BowTiedDevil <devil@bowtieddevil.com>
2026-02-09 11:27:12 -0800

    feat: `DEGENBOT_DEBUG` env var

--- src/degenbot/logging.py
+++ src/degenbot/logging.py
@@ -1,4 +1,5 @@
 import logging
+import os
 import sys
 
 """
@@ -7,5 +8,11 @@ Create a global logger instance.
 
 logger = logging.getLogger(__name__)
 logger.propagate = False
-logger.setLevel(logging.INFO)
+
+# Check DEGENBOT_DEBUG environment variable for debug mode
+if os.environ.get("DEGENBOT_DEBUG", "").lower() in {"1", "true", "yes"}:
+    logger.setLevel(logging.DEBUG)
+else:
+    logger.setLevel(logging.INFO)
+
 logger.addHandler(logging.StreamHandler(sys.stdout))
