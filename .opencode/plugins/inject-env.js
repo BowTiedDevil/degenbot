10f39415b3cbe6a990c582ece5237142733636b7
Ralph <ralph@bowtieddevil.com>
2026-03-07 15:01:58 -0800

    agent: update envvars

--- .opencode/plugins/inject-env.js
+++ .opencode/plugins/inject-env.js
@@ -2,6 +2,7 @@ export const InjectEnvPlugin = async () => {
   return {
     "shell.env": async (input, output) => {
       output.env.DEGENBOT_PROGRESS_BAR = "false"
+      output.env.DEGENBOT_ONE_CHUNK = "true"
     },
   }
 }
