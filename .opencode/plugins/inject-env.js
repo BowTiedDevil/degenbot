7a4cb72c316b180b0409e5d8de7127b4f2f9fdcb
Ralph <ralph@bowtieddevil.com>
2026-03-10 12:38:04 -0700

    feat: inject debug envvar

--- .opencode/plugins/inject-env.js
+++ .opencode/plugins/inject-env.js
@@ -1,6 +1,7 @@
 export const InjectEnvPlugin = async () => {
   return {
     "shell.env": async (input, output) => {
+      output.env.DEGENBOT_DEBUG = "true"
       output.env.DEGENBOT_PROGRESS_BAR = "false"
       output.env.DEGENBOT_ONE_CHUNK = "true"
     },
