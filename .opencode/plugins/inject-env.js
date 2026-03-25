391bcc73794ef90b32a12c84fa23c16877b0572e
Ralph <ralph@bowtieddevil.com>
2026-03-25 16:37:57 -0700

    chore: update envvar

--- .opencode/plugins/inject-env.js
+++ .opencode/plugins/inject-env.js
@@ -5,11 +5,11 @@ export const InjectEnvPlugin = async () => {
       output.env.DEGENBOT_PROGRESS_BAR = "false"
       output.env.DEGENBOT_ONE_CHUNK = "true"
       output.env.DEGENBOT_COVERAGE = "true"
-//      output.env.DEGENBOT_CHUNK_SIZE = "1"
+      //      output.env.DEGENBOT_CHUNK_SIZE = "1"
       output.env.DEGENBOT_VERIFY_ALL = "true"
       output.env.DEGENBOT_VERIFY_BLOCK = "true"
       output.env.DEGENBOT_VERIFY_CHUNK = "true"
-      output.env.OPENCODE_EXPERIMENTAL_BASH_DEFAULT_TIMEOUT_MS = "1800000"
+      output.env.OPENCODE_EXPERIMENTAL_BASH_DEFAULT_TIMEOUT_MS = 1800000
     },
   }
 }
