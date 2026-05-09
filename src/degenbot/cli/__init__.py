9407a73824ce1fa40776bb1c7cd04629dc107cf3
Ralph <ralph@bowtieddevil.com>
2026-05-09 08:00:39 -0700

    fix: CLI context

--- src/degenbot/cli/__init__.py
+++ src/degenbot/cli/__init__.py
@@ -7,8 +7,7 @@ from degenbot.bot import Bot
 @click.version_option()
 @click.pass_context
 def cli(ctx: click.Context) -> None:
-    ctx.ensure_object(dict)
-    ctx.obj["bot"] = Bot.from_config_file()
+    ctx.obj = Bot.from_config_file()
 
 
 from . import aave, database, exchange, pool  # noqa: F401, E402
