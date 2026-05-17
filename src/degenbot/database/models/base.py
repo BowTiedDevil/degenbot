e0ecb34cc24b7753a20e57e835f8151f9fb2a027
Ralph <ralph@bowtieddevil.com>
2026-05-16 21:52:10 -0700

    lint: cleanup

--- src/degenbot/database/models/base.py
+++ src/degenbot/database/models/base.py
@@ -21,7 +21,7 @@ class IntMappedToString(TypeDecorator[int]):
     def process_bind_param(
         self,
         value: int | None,
-        dialect: Dialect,
+        dialect: Dialect,  # ruff: ignore[ARG002]
     ) -> str | None:
         """
         Perform the Python type -> DB type conversion.
@@ -32,7 +32,7 @@ class IntMappedToString(TypeDecorator[int]):
     def process_result_value(
         self,
         value: str | None,
-        dialect: Dialect,
+        dialect: Dialect,  # ruff: ignore[ARG002]
     ) -> int | None:
         """
         Perform the DB type -> Python type conversion.
