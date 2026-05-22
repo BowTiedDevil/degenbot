86413078860ca8221d1e664a481b0609070d9ca3
Ralph <ralph@bowtieddevil.com>
2026-05-22 12:18:44 -0700

    lint: fix uppercase names

--- src/degenbot/curve/data_provider_impl.py
+++ src/degenbot/curve/data_provider_impl.py
@@ -164,7 +164,7 @@ class CurveDataProviderImpl:
                 break
         return tuple(balances)
 
-    def D(self, block_number: int) -> int:
+    def d(self, block_number: int) -> int:
         return cast(
             "int",
             self._wrap_revert(
