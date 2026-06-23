f52e6f336ea58ec84dc5847deaf9ecfc92b47651
Ralph <ralph@bowtieddevil.com>
2026-06-23 02:08:18 -0700

    chore: dead code removal

--- src/degenbot/uniswap/v3_libraries/unsafe_math.py
+++
@@ -1,8 +0,0 @@
-"""Uniswap V3 UnsafeMath: rounding-up division.
-
-Rust-accelerated implementation used by default.
-
-See: contract_reference/uniswap/V3/UniswapV3Factory.sol (UnsafeMath library)
-"""
-
-from degenbot.degenbot_rs import cl_div_rounding_up as div_rounding_up  # noqa: F401
